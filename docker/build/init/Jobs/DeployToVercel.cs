9014ab05e651d819600f360c8c4e89b2fde10f0c
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-06-15 17:16:32 -0400

    Added DeployToVercel job

---
+++ docker/build/init/Jobs/DeployToVercel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Sitecore.Demo.Init.Jobs
+{
+	using Microsoft.Extensions.Logging;
+
+	class DeployToVercel : TaskBase
+	{
+		public DeployToVercel(InitContext initContext)
+			: base(initContext)
+		{
+		}
+
+		public async Task Run()
+		{
+            if (this.IsCompleted())
+            {
+                Log.LogWarning($"{this.GetType().Name} is already complete, it will not execute this time");
+                return;
+            }
+
+            var token = Environment.GetEnvironmentVariable("VERCEL_TOKEN");
+            if (string.IsNullOrEmpty(token))
+            {
+                Log.LogWarning($"{this.GetType().Name} will not execute this time, VERCEL_TOKEN is not configured");
+                return;
+			}
+
+			var cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.WorkingDirectory = "/website";
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine($"vercel --confirm --prod --token {token}");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+			await Complete();
+		}
+	}
+}
