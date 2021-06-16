11b54b976079e809252455ce96f52b239112a2af
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-06-16 15:29:00 -0400

    Update DeployToVercel.cs

--- docker/build/init/Jobs/DeployToVercel.cs
+++ docker/build/init/Jobs/DeployToVercel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Sitecore.Demo.Init.Container;
 
@@ -35,11 +36,19 @@ namespace Sitecore.Demo.Init.Jobs
 
             // Configure env. variables
             cmd.Run($"echo {cm} | vercel env add SITECORE_API_HOST production --token {token}");
-            cmd.Run($"echo {{1047AEE5-9BCD-4DBF-9744-A26E12B79AB6}} | vercel env add SITECORE_API_KEY production  --token {token}");
+            cmd.Run($"echo {{1047AEE5-9BCD-4DBF-9744-A26E12B79AB6}} | vercel env add SITECORE_API_KEY production --token {token}");
             cmd.Run($"echo 7QF3bkaKFD2EdFHqluHeRbi6ZjoQYXqQUrgonMQfdEwFqDHjY7Z55oaxeMRAFqHY | vercel env add JSS_EDITING_SECRET production --token {token}");
 
             // Deploy project files
-            cmd.Run($"vercel --confirm --debug --prod --no-clipboard --token {token} --env SITECORE_API_HOST={cm} --env SITECORE_API_KEY={{1047AEE5-9BCD-4DBF-9744-A26E12B79AB6}}");
+            var response = cmd.Run($"vercel --confirm --debug --prod --no-clipboard --token {token} --env SITECORE_API_HOST={cm} --env SITECORE_API_KEY={{1047AEE5-9BCD-4DBF-9744-A26E12B79AB6}}");
+            var productionUrl = response.Split(Environment.NewLine).FirstOrDefault(x => x.StartsWith("Production:"));
+            if (!string.IsNullOrEmpty(productionUrl))
+            {
+                Log.LogWarning($"Production Url string: {productionUrl}");
+                productionUrl = productionUrl.Split(" ")[1].Trim();
+            }
+
+            cmd.Run($"echo {productionUrl} | vercel env add PUBLIC_URL production --token {token}");
 
             await Complete();
 		}
