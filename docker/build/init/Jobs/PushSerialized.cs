2beb0aeb48674942c1bb32bb1eac34a271b2a6c0
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-06-18 14:03:58 -0400

    Update PushSerialized.cs

--- docker/build/init/Jobs/PushSerialized.cs
+++ docker/build/init/Jobs/PushSerialized.cs
@@ -45,7 +45,8 @@ namespace Sitecore.Demo.Init.Jobs
 
             var cmd = new WindowsCommandLine("C:\\app");
             cmd.Run($"dotnet sitecore login --client-credentials true --auth {id} --cm {cm} --allow-write true --client-id \"Demo_Automation\" --client-secret \"{token}\" -t");
-            cmd.Run($"dotnet sitecore ser push --publish -t");
+            cmd.Run($"dotnet sitecore ser push");
+            cmd.Run($"dotnet sitecore publish");
 
             await Complete();
 		}
