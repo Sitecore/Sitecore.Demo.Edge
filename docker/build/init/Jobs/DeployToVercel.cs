506c12604dee697d1ad4f4ed5dc0719cf4d71906
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-06-25 10:25:39 -0400

    AB#6215 Added Vercel scope parameter

--- docker/build/init/Jobs/DeployToVercel.cs
+++ docker/build/init/Jobs/DeployToVercel.cs
@@ -31,6 +31,13 @@ namespace Sitecore.Demo.Init.Jobs
                 return;
             }
 
+            var scope = Environment.GetEnvironmentVariable("VERCEL_SCOPE");
+            if (string.IsNullOrEmpty(token))
+            {
+                Log.LogWarning($"{this.GetType().Name} will not execute this time, VERCEL_SCOPE is not configured");
+                return;
+            }
+
             var cm = Environment.GetEnvironmentVariable("PUBLIC_HOST_CM");
             var ns = Environment.GetEnvironmentVariable("RELEASE_NAMESPACE");
             var js = Environment.GetEnvironmentVariable("SITECORE_JSS_EDITING_SECRET");
@@ -41,23 +48,17 @@ namespace Sitecore.Demo.Init.Jobs
             Directory.Move(sourceDirectory, targetDirectory);
 
             var cmd = new WindowsCommandLine(targetDirectory);
-
             cmd.Run($"vercel link --confirm --token {token} --debug");
-
-            // Hack until domain mgmt. clarified
-            var whoami = cmd.Run($"vercel whoami --token {token}").Split(Environment.NewLine)[4].Trim();
-            var productionUrl = $"https://rendering-{ns}-{whoami}.vercel.app";
+            var productionUrl = $"https://rendering-{ns}-{scope}.vercel.app";
 
             // Configure env. variables
-            cmd.Run($"echo | set /p=\"{productionUrl}\" | vercel env add PUBLIC_URL production --token {token}");
-            cmd.Run($"echo | set /p=\"{cm}\" | vercel env add SITECORE_API_HOST production --token {token}");
-            cmd.Run($"echo | set /p=\"{SitecoreApiKey}\" | vercel env add SITECORE_API_KEY production --token {token}");
-            cmd.Run($"echo | set /p=\"{js}\" | vercel env add JSS_EDITING_SECRET production --token {token}");
+            cmd.Run($"echo | set /p=\"{productionUrl}\" | vercel env add PUBLIC_URL production --token {token} --scope {scope}");
+            cmd.Run($"echo | set /p=\"{cm}\" | vercel env add SITECORE_API_HOST production --token {token} --scope {scope}");
+            cmd.Run($"echo | set /p=\"{SitecoreApiKey}\" | vercel env add SITECORE_API_KEY production --token {token} --scope {scope}");
+            cmd.Run($"echo | set /p=\"{js}\" | vercel env add JSS_EDITING_SECRET production --token {token} --scope {scope}");
 
             // Deploy project files
-            var response =
-                cmd.Run(
-                    $"vercel --confirm --debug --prod --no-clipboard --token {token} --env SITECORE_API_HOST={cm} --env SITECORE_API_KEY={SitecoreApiKey}");
+            var response = cmd.Run($"vercel --confirm --debug --prod --no-clipboard --token {token} --scope {scope}");
             Console.WriteLine($"Log lines: {response.Split(Environment.NewLine).Length}");
 
             await Complete();
