12c7a805c56dbb3f79c2e2e4f87676edb0f714d7
Minhaz Topaz <mint@sitecore.net>
2021-07-05 14:10:47 -0600

    undo changes

--- Website/src/rendering/sitecore/definitions/components/HeroSection.sitecore.ts
+++ Website/src/rendering/sitecore/definitions/components/HeroSection.sitecore.ts
@@ -10,14 +10,7 @@ const HeroSection = function (manifest: Manifest): void {
   manifest.addComponent({
     name: 'HeroSection',
     icon: SitecoreIcon.DocumentTag,
-    fields: [
-      { name: 'eyebrow', type: CommonFieldTypes.SingleLineText },
-      { name: 'title', type: CommonFieldTypes.SingleLineText },
-      { name: 'subtitle', type: CommonFieldTypes.SingleLineText },
-      { name: 'content', type: CommonFieldTypes.SingleLineText },
-      { name: 'subcontent', type: CommonFieldTypes.SingleLineText },
-      { name: 'callToActionLink', type: CommonFieldTypes.GeneralLink },
-    ],
+    fields: [{ name: 'heading', type: CommonFieldTypes.SingleLineText }],
     /*
     If the component implementation uses <Placeholder> or withPlaceholder to expose a placeholder,
     register it here, or components added to that placeholder will not be returned by Sitecore:
