7f70bb69f3249759014da23d4cd972048c2c9b6f
Minhaz Topaz <mint@sitecore.net>
2021-07-05 11:34:30 -0600

    hero section refactor

--- Website/src/rendering/sitecore/definitions/components/HeroSection.sitecore.ts
+++ Website/src/rendering/sitecore/definitions/components/HeroSection.sitecore.ts
@@ -10,7 +10,14 @@ const HeroSection = function (manifest: Manifest): void {
   manifest.addComponent({
     name: 'HeroSection',
     icon: SitecoreIcon.DocumentTag,
-    fields: [{ name: 'heading', type: CommonFieldTypes.SingleLineText }],
+    fields: [
+      { name: 'eyebrow', type: CommonFieldTypes.SingleLineText },
+      { name: 'title', type: CommonFieldTypes.SingleLineText },
+      { name: 'subtitle', type: CommonFieldTypes.SingleLineText },
+      { name: 'content', type: CommonFieldTypes.SingleLineText },
+      { name: 'subcontent', type: CommonFieldTypes.SingleLineText },
+      { name: 'callToActionLink', type: CommonFieldTypes.GeneralLink },
+    ],
     /*
     If the component implementation uses <Placeholder> or withPlaceholder to expose a placeholder,
     register it here, or components added to that placeholder will not be returned by Sitecore:
--- Website/src/rendering/src/components/HeroSection.tsx
+++ Website/src/rendering/src/components/HeroSection.tsx
@@ -1,27 +1,46 @@
-import Link from 'next/link';
+import { Text, RichText, Field, Link, LinkField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
 
-const HeroSection = (): JSX.Element => (
-  <section className="section__hero banner">
-    <div className="section__hero__container">
-      <div className="section__hero__container__content">
-        <div className="section__hero__container__content__text">
-          <img src="/assets/img/play-logo-wide-light.svg" alt="PLAY! Summit" />
-          <p className="banner__title__eyebrow">Ready | Steady | Play!</p>
-          <h1 className="banner__title__sub-title">Sports and Leisure Expo</h1>
-          <h3 className="banner__title__title">Raise Your Game</h3>
-          <p className="banner__title__p">
-            Join us in person or online for the fifth annual PLAY! Summit.
-          </p>
-          <h3 className="banner__title__sub-title">August 24th – 25th</h3>
-        </div>
-        <div className="btn__area">
-          <Link href="/tickets">
-            <a className="btn--main btn--main--round btn--main--big">Book tickets</a>
-          </Link>
+type HeroSectionProps = ComponentProps & {
+  fields: {
+    eyebrow: Field<string>;
+    title: Field<string>;
+    subtitle: Field<string>;
+    content: Field<string>;
+    subcontent: Field<string>;
+    callToActionLink: LinkField;
+  };
+};
+
+const HeroSection = (props: HeroSectionProps): JSX.Element => {
+  const eyebrowCssClasses = `banner__title__eyebrow`;
+  const titleCssClasses = `banner__title__title`;
+  const subtitleCssClasses = `banner__title__sub-title`;
+  const contentCssClasses = `banner__title__p`;
+  const subcontentCssClasses = `banner__title__sub-title`;
+
+  return (
+    <section className="section__hero banner">
+      <div className="section__hero__container">
+        <div className="section__hero__container__content">
+          <div className="section__hero__container__content__text">
+            <img src="/assets/img/play-logo-wide-light.svg" alt="PLAY! Summit" />
+            <RichText tag="p" field={props.fields.eyebrow} className={eyebrowCssClasses} />
+            <Text tag="h1" field={props.fields.subtitle} className={subtitleCssClasses} />
+            <Text tag="h3" field={props.fields.title} className={titleCssClasses} />
+            <RichText tag="p" field={props.fields.content} className={contentCssClasses} />
+            <Text tag="h3" field={props.fields.subcontent} className={subcontentCssClasses} />
+          </div>
+          <div className="btn__area">
+            <Link
+              field={props.fields.callToActionLink}
+              className="btn--main btn--main--round btn--main--big"
+            ></Link>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default HeroSection;
