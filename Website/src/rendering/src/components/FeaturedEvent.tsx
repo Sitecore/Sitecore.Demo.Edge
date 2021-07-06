fd885634ed242b9b80d541c0e78d48e4929ed317
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-06 13:32:04 -0400

    Update FeaturedEvent.tsx

--- Website/src/rendering/src/components/FeaturedEvent.tsx
+++ Website/src/rendering/src/components/FeaturedEvent.tsx
@@ -1,19 +1,10 @@
-import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
-import { ComponentProps } from 'lib/component-props';
 import Link from 'next/link';
 
-type FeaturedEventProps = ComponentProps & {
-  fields: {
-    heading: Field<string>;
-  };
-};
-
-const FeaturedEvent = (props: FeaturedEventProps): JSX.Element => (
+const FeaturedEvent = (): JSX.Element => (
   <section className="section__featured__event mt-20 banner bg-black bg-left bg-no-repeat bg-cover relative">
     <div className="px-14 max-w-screen-2xl flex items-center justify-end w-full my-0 mx-auto box-border overflow-hidden">
       <div className="triangle max-w-30 relative shadow-xl w-full bg-black-light">
         <div className="p-14">
-          <Text field={props.fields.heading} />
           <h1 className="text-2xl text-yellow font-semibold">Featured Event</h1>
           <h3 className="text-5xl font-semibold text-white uppercase m-0">STRIVA</h3>
           <h3 className="text-2xl text-yellow font-semibold">Mon, 24th | 9:00 AM</h3>
