d4579f470abc98e365241c911a65e555653ab348
Minhaz Topaz <mint@sitecore.net>
2021-07-06 15:44:23 -0600

    header banner added

---
+++ Website/src/rendering/src/components/HeaderBanner.tsx
@@ -0,0 +1,18 @@
+import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+type HeaderBannerProps = ComponentProps & {
+  fields: {
+    heading: Field<string>;
+  };
+};
+
+const HeaderBanner = (props: HeaderBannerProps): JSX.Element => (
+  <section className="section section__full-image">
+    <div className="section__content section__full-image__content section__full-image__content--center">
+      <Text className="bottom-0 absolute left-0" tag="h2" field={props.fields.heading} />
+    </div>
+  </section>
+);
+
+export default HeaderBanner;
