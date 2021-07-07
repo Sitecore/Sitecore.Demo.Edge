2e7c5a1a01c497410d3304472bd039a6aeb02641
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-06 20:09:49 -0400

    Refactoring

--- Website/src/rendering/src/components/SessionsFilters.tsx
+++ Website/src/rendering/src/components/SessionsFilters.tsx
@@ -1,18 +1,18 @@
 const SessionsFilters = (): JSX.Element => (
   <section>
     <div className="max-w-screen-2xl my-0 mx-auto box-border overflow-hidden">
-      <div className="px-10 grid grid-cols-2 gap-4 flex">
-        <div className="">
+      <div className="px-10 flex flex-wrap overflow-hidden">
+        <div className="w-full overflow-hidden sm:w-full md:w-1/2">
           <input
             className="w-full md:w-3/5 rounded p-2 border border-white-dark text-sm font-medium"
             type="text"
             placeholder="Search sessions..."
           />
         </div>
-        <div className="flex justify-end">
+        <div className="w-full overflow-hidden sm:w-full md:w-1/2 flex justify-start md:justify-end">
           <button
             type="button"
-            className="inline-flex justify-center w-40 border border-white-dark shadow-sm px-4 py-2 bg-white-light text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+            className="hidden md:inline-flex justify-center w-40 border border-white-dark shadow-sm px-4 py-2 bg-white-light text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
             id="menu-button"
             aria-expanded="true"
             aria-haspopup="true"
@@ -35,41 +35,39 @@ const SessionsFilters = (): JSX.Element => (
         </div>
       </div>
 
-      <div className="px-10 grid grid-cols-1 md:grid-cols-2 gap-4 flex pt-5">
-        <div className="flex">
+      <div className="px-10 flex flex-wrap overflow-hidden pt-5">
+        <div className="w-full overflow-hidden sm:w-full md:w-1/2 flex justify-between md:justify-start">
           <div className="pr-5 py-1 text-sm hidden md:block">
             <p>214 sessions</p>
           </div>
-          <div>
-            <button
-              type="button"
-              className="inline-flex justify-center w-30 border border-white-dark shadow-sm px-4 py-1 bg-white-light text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-              id="menu-button"
-              aria-expanded="true"
-              aria-haspopup="true"
+          <button
+            type="button"
+            className="inline-flex justify-center w-30 border border-white-dark shadow-sm px-4 py-1 bg-white-light text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+            id="menu-button"
+            aria-expanded="true"
+            aria-haspopup="true"
+          >
+            Chronological
+            <svg
+              className="-mr-1 ml-2 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
             >
-              Chronological
-              <svg
-                className="-mr-1 ml-2 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-            <label className="inline-flex items-center pl-5">
-              <input type="checkbox" className="form-checkbox" defaultChecked />
-              <span className="ml-2 text-sm">Space available</span>
-            </label>
-          </div>
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          <label className="inline-flex items-center pl-5">
+            <input type="checkbox" className="form-checkbox" defaultChecked />
+            <span className="ml-2 text-sm">Space available</span>
+          </label>
         </div>
-        <div className="flex justify-end">
+        <div className="w-full overflow-hidden sm:w-full md:w-1/2 flex pt-4 md:pt-0 justify-start md:justify-end">
           <div className="flex items-center justify-between">
             <div className="sm:flex-1 sm:flex sm:items-center sm:justify-between">
               <div>
