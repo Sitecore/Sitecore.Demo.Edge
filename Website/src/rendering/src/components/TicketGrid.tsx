07fffed3b306f82c14b9bebc4eb8dd180330b4a1
Minhaz Topaz <mint@sitecore.net>
2021-07-07 17:47:56 -0600

    move pages to match IA

--- Website/src/rendering/src/components/TicketGrid.tsx
+++ Website/src/rendering/src/components/TicketGrid.tsx
@@ -18,7 +18,7 @@ const TicketGrid = (): JSX.Element => (
         </ul>
       </p>
       <div className="px-6 pt-4 pb-10">
-        <Link href="/tickets/registration">
+        <Link href="/tickets/attendee">
           <a className="btn--main btn--main--round">Get Tickets</a>
         </Link>
       </div>
@@ -39,7 +39,7 @@ const TicketGrid = (): JSX.Element => (
         </ul>
       </p>
       <div className="px-6 pt-4 pb-10">
-        <Link href="/tickets/registration">
+        <Link href="/tickets/attendee">
           <a className="btn--main btn--main--round">Get Tickets</a>
         </Link>
       </div>
@@ -60,7 +60,7 @@ const TicketGrid = (): JSX.Element => (
         </ul>
       </p>
       <div className="px-6 pt-4 pb-10">
-        <Link href="/tickets/registration">
+        <Link href="/tickets/attendee">
           <a className="btn--main btn--main--round">Get Tickets</a>
         </Link>
       </div>
