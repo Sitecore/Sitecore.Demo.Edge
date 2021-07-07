7ca197af68afef468f0f3c36c2b70430fcca80c7
Minhaz Topaz <mint@sitecore.net>
2021-07-07 12:44:52 -0600

    remove unused field reference

--- Website/src/rendering/src/components/AgendaGrid.tsx
+++ Website/src/rendering/src/components/AgendaGrid.tsx
@@ -1,16 +1,6 @@
-import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
-import { ComponentProps } from 'lib/component-props';
-
-type AgendaGridProps = ComponentProps & {
-  fields: {
-    heading: Field<string>;
-  };
-};
-
-const AgendaGrid = (props: AgendaGridProps): JSX.Element => (
+const AgendaGrid = (): JSX.Element => (
   <div>
     <p>AgendaGrid Component</p>
-    <Text field={props.fields.heading} />
   </div>
 );
 
--- Website/src/rendering/src/components/GalleryGrid.tsx
+++ Website/src/rendering/src/components/GalleryGrid.tsx
@@ -1,16 +1,6 @@
-import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
-import { ComponentProps } from 'lib/component-props';
-
-type GalleryGridProps = ComponentProps & {
-  fields: {
-    heading: Field<string>;
-  };
-};
-
-const GalleryGrid = (props: GalleryGridProps): JSX.Element => (
+const GalleryGrid = (): JSX.Element => (
   <div>
     <p>GalleryGrid Component</p>
-    <Text field={props.fields.heading} />
   </div>
 );
 
