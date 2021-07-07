5ec9130e7d873ecdc912e220d3be762bd630a7fa
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-07 11:29:35 -0400

    Added Vendors page

---
+++ Website/src/rendering/src/components/HeroImage.tsx
@@ -0,0 +1,8 @@
+const HeroImage = (): JSX.Element => (
+  <section
+  className="mt-20 banner bg-black bg-left bg-no-repeat bg-cover relative h-96"
+  style={{ backgroundImage: 'url("/assets/img/swimmer.jpg")' }}
+></section>    
+);
+
+export default HeroImage;
