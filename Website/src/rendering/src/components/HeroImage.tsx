b8d4451c616eb845918b3675eabddb79e7fa93fc
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-07 11:46:22 -0400

    Update HeroImage.tsx

--- Website/src/rendering/src/components/HeroImage.tsx
+++ Website/src/rendering/src/components/HeroImage.tsx
@@ -1,8 +1,8 @@
 const HeroImage = (): JSX.Element => (
   <section
-  className="mt-20 banner bg-black bg-left bg-no-repeat bg-cover relative h-96"
-  style={{ backgroundImage: 'url("/assets/img/swimmer.jpg")' }}
-></section>    
+    className="mt-20 banner bg-black bg-left bg-no-repeat bg-cover relative h-96"
+    style={{ backgroundImage: 'url("/assets/img/swimmer.jpg")' }}
+  ></section>
 );
 
 export default HeroImage;
