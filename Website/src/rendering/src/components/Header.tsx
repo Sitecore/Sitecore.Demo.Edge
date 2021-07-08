ffc8f3fb931f78a59933c35853d54f1a4af6a7f1
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-08 12:51:21 -0400

    Refactoring

--- Website/src/rendering/src/components/Header.tsx
+++ Website/src/rendering/src/components/Header.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import { Field, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
 import Link from 'next/link';
 
-const Header = (): JSX.Element => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
+type HeaderProps = ComponentProps & {
+  fields: {
+    heading: Field<string>;
+  };
+};
+
+const Header = (props: HeaderProps): JSX.Element => {
   return (
     <>
       <div className="bg-black-lightest text-white">
@@ -18,97 +25,7 @@ const Header = (): JSX.Element => {
           </Link>
         </div>
       </div>
-      <nav
-        className="relative flex flex-wrap items-center justify-between mb-3 bg-black bg-gradient-to-l from-black to-black-light"
-        style={{ height: '72px' }}
-      >
-        <div className="w-full lg:container lg:mx-auto flex flex-wrap items-center justify-between">
-          <div
-            className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start px-6"
-            style={{ height: '72px' }}
-          >
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="/"
-            >
-              <img
-                src="/assets/img/play-logo-stacked-light.svg"
-                alt="PLAY! Summit logo"
-                style={{ height: '46px' }}
-              />
-            </a>
-            <button
-              className="text-white cursor-pointer text-xl leading-none border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none w-8"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div
-            className={
-              'lg:flex flex-grow bg-black-light lg:bg-transparent px-4 pb-5 lg:pb-0' +
-              (navbarOpen ? ' flex' : ' hidden')
-            }
-          >
-            <ul className="flex flex-col lg:flex-row lg:items-center list-none lg:ml-auto">
-              <li className="nav-item">
-                <Link href="/schedule">
-                  <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
-                    Schedule
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/speakers">
-                  <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
-                    Speakers
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/vendors">
-                  <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
-                    Vendors
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/map">
-                  <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
-                    Map
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/shop">
-                  <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
-                    Shop
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/news">
-                  <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
-                    News
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/tickets">
-                  <a className="btn--main btn--main--round inline-block text-black">Book Tickets</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Placeholder name="jss-header-content" rendering={props.rendering} />
     </>
   );
 };
--- Website/src/rendering/src/components/MainNavigation.tsx
+++ Website/src/rendering/src/components/MainNavigation.tsx
@@ -1,17 +1,102 @@
 import Link from 'next/link';
+import React, { useState } from 'react';
 
-const MainNavigation = (): JSX.Element => (
-  <nav id="topNav" className="header__container__content__menu text-white">
-    <Link href="/schedule">Schedule</Link>
-    <Link href="/speakers">Speakers</Link>
-    <Link href="/vendors">Vendors</Link>
-    <Link href="/map">Map</Link>
-    <Link href="/shop">Shop</Link>
-    <Link href="/news">News</Link>
-    <Link href="/tickets">
-      <a className="btn--main btn--main--round text-black">Book Tickets</a>
-    </Link>
-  </nav>
-);
+const MainNavigation = (): JSX.Element => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav
+      className="relative flex flex-wrap items-center justify-between mb-3 bg-black bg-gradient-to-l from-black to-black-light"
+      style={{ height: '72px' }}
+    >
+      <div className="w-full lg:container lg:mx-auto flex flex-wrap items-center justify-between">
+        <div
+          className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start px-6"
+          style={{ height: '72px' }}
+        >
+          <a
+            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+            href="/"
+          >
+            <img
+              src="/assets/img/play-logo-stacked-light.svg"
+              alt="PLAY! Summit logo"
+              style={{ height: '46px' }}
+            />
+          </a>
+          <button
+            className="text-white cursor-pointer text-xl leading-none border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none w-8"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className={
+            'lg:flex flex-grow bg-black-light lg:bg-transparent px-4 pb-5 lg:pb-0' +
+            (navbarOpen ? ' flex' : ' hidden')
+          }
+        >
+          <ul className="flex flex-col lg:flex-row lg:items-center list-none lg:ml-auto">
+            <li className="nav-item">
+              <Link href="/schedule">
+                <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
+                  Schedule
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/speakers">
+                <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
+                  Speakers
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/vendors">
+                <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
+                  Vendors
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/map">
+                <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
+                  Map
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/shop">
+                <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
+                  Shop
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/news">
+                <a className="px-3 py-2 text-base leading-loose text-white hover:text-yellow">
+                  News
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/tickets">
+                <a className="btn--main btn--main--round inline-block text-black">Book Tickets</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default MainNavigation;
