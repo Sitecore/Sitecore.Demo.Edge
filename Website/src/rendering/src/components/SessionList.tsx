01eab86f52cec4b19eb37ef9e738e6ae9adc1fd7
Minhaz Topaz <mint@sitecore.net>
2021-07-08 15:40:15 -0600

    add fontawesome and vendor info component

---
+++ Website/src/rendering/src/components/SessionList.tsx
@@ -0,0 +1,7 @@
+const SessionList = (): JSX.Element => (
+  <div>
+    <p>SessionList Component</p>
+  </div>
+);
+
+export default SessionList;
--- Website/src/rendering/src/components/VendorInformation.tsx
+++ Website/src/rendering/src/components/VendorInformation.tsx
@@ -1,17 +1,44 @@
-import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
-import { ComponentProps } from 'lib/component-props';
+import {
+  faFacebookF,
+  faTwitter,
+  faLinkedinIn,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-type VendorInformationProps = ComponentProps & {
-  fields: {
-    heading: Field<string>;
-  };
-};
-
-const VendorInformation = (props: VendorInformationProps): JSX.Element => (
-  <div>
-    <p>VendorInformation Component</p>
-    <Text field={props.fields.heading} />
-  </div>
+const VendorInformation = (): JSX.Element => (
+  <section className="section">
+    <div className="section__content left__content">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
+        <div className="col-span-1 md:col-span-1">
+          <img className="border border-gray" src="/assets/img/logo/striva.svg" alt="striva logo" />
+          <div>
+            <FontAwesomeIcon className="icon" icon={faFacebookF} />
+            <FontAwesomeIcon className="icon" icon={faTwitter} />
+            <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
+            <FontAwesomeIcon className="icon" icon={faInstagram} />
+          </div>
+        </div>
+        <div className="col-span-1 md:col-span-3 space-y-5">
+          <h2>Striva</h2>
+          <p className="text-base">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed.
+          </p>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+            sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+            amet. At vero eos et accusam et justo.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
 export default VendorInformation;
