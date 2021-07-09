fc24ead5663afe8ccc3c0a3346923dae4ff47b38
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-09 16:24:48 -0400

    Use next/image

--- Website/src/rendering/src/components/SponsorsGrid.tsx
+++ Website/src/rendering/src/components/SponsorsGrid.tsx
@@ -1,35 +1,48 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const SponsorsGrid = (): JSX.Element => (
   <div className="section__sponsors__grid">
     <Link href="/sponsors/fitbit">
       <a className="section__sponsors__grid__sponsor">
-        <img src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" />
+        <div className="m-3">
+          <Image src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" width={180} height={80} />
+        </div>
       </a>
     </Link>
     <Link href="/sponsors/sports">
       <a className="section__sponsors__grid__sponsor">
-        <img src="/assets/img/sponsors-sports.svg" alt="Sports" />
+        <div className="m-3">
+          <Image src="/assets/img/sponsors-sports.svg" alt="Sports" width={180} height={80} />
+        </div>
       </a>
     </Link>
     <Link href="/sponsors/fitbit">
       <a className="section__sponsors__grid__sponsor">
-        <img src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" />
+        <div className="m-3">
+          <Image src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" width={180} height={80} />
+        </div>
       </a>
     </Link>
     <Link href="/sponsors/sports">
       <a className="section__sponsors__grid__sponsor">
-        <img src="/assets/img/sponsors-sports.svg" alt="Sports" />
+        <div className="m-3">
+          <Image src="/assets/img/sponsors-sports.svg" alt="Sports" width={180} height={80} />
+        </div>
       </a>
     </Link>
     <Link href="/sponsors/fitbit">
       <a className="section__sponsors__grid__sponsor">
-        <img src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" />
+        <div className="m-3">
+          <Image src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" width={180} height={80} />
+        </div>
       </a>
     </Link>
     <Link href="/sponsors/sports">
       <a className="section__sponsors__grid__sponsor">
-        <img src="/assets/img/sponsors-sports.svg" alt="Sports" />
+        <div className="m-3">
+          <Image src="/assets/img/sponsors-sports.svg" alt="Sports" width={180} height={80} />
+        </div>
       </a>
     </Link>
   </div>
