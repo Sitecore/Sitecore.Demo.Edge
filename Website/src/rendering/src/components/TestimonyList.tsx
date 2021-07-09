b2e60a31300c61a1831f5e47d6a00014b3e23b27
Minhaz Topaz <mint@sitecore.net>
2021-07-09 15:58:28 -0600

    forms

--- Website/src/rendering/src/components/TestimonyList.tsx
+++ Website/src/rendering/src/components/TestimonyList.tsx
@@ -1,7 +1,7 @@
 const TestimonyList = (): JSX.Element => (
   <div className="content-list w-full p-10">
     <div className="Content-block h-40 w-full text-left flex flex-row gap-10">
-      <div>
+      <div className="hidden md:block">
         <img className="image h-30 w-40 mr-10" src="/assets/img/logo/alba.svg" alt=""></img>
       </div>
       <div>
@@ -15,7 +15,9 @@ const TestimonyList = (): JSX.Element => (
       </div>
     </div>
     <div className="Content-block h-40 w-full text-right flex flex-row-reverse gap-10">
-      <img className="image h-20 w-30 ml-10" src="/assets/img/logo/striva.svg" alt=""></img>
+      <div className="hidden md:block">
+        <img className="image h-20 w-30 ml-10" src="/assets/img/logo/striva.svg" alt=""></img>
+      </div>
       <p>
         &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales mi in magna
         accumsan, vitae finibus libero fringilla. Duis posuere a lorem quis pretium. Sed varius
@@ -25,7 +27,9 @@ const TestimonyList = (): JSX.Element => (
       </p>
     </div>
     <div className="Content-block h-40 w-full text-left flex flex-row gap-10">
-      <img className="image h-20 w-30 mr-10" src="/assets/img/logo/onthegreen.svg" alt=""></img>
+      <div className="hidden md:block">
+        <img className="image h-20 w-30 mr-10" src="/assets/img/logo/onthegreen.svg" alt=""></img>
+      </div>
       <p>
         &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales mi in magna
         accumsan, vitae finibus libero fringilla. Duis posuere a lorem quis pretium. Sed varius
