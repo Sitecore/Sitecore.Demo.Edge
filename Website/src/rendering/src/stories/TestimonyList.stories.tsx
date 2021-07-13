aa5166eee15f75a8940c12c4f26dfad9a8267ee8
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-13 14:21:27 -0400

    Added more stories

---
+++ Website/src/rendering/src/stories/TestimonyList.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import TestimonyList  from '../components/TestimonyList';
+
+export default {
+  title: 'Example/TestimonyList',
+  component: TestimonyList,
+} as ComponentMeta<typeof TestimonyList>;
+
+const Template: ComponentStory<typeof TestimonyList> = () => <TestimonyList />;
+
+export const Default = Template.bind({});
+Default.args = {};
