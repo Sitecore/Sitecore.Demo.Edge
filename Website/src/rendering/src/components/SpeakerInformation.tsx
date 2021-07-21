db47879245adddb0de4ec43212eba06a5dded353
Minhaz Topaz <mint@sitecore.net>
2021-07-21 12:05:16 -0600

    update sitecore items

--- Website/src/rendering/src/components/SpeakerInformation.tsx
+++ Website/src/rendering/src/components/SpeakerInformation.tsx
@@ -5,54 +5,62 @@ import {
   faInstagram,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ComponentProps } from 'lib/component-props';
+import { Field, ImageField, Image, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
 
-const SpeakerInformation = (): JSX.Element => (
+type SpeakerInformationProps = ComponentProps & {
+  fields: {
+    name: Field<string>;
+    Image: ImageField;
+    Position: Field<string>;
+    Company: Field<string>;
+    Country: Field<string>;
+    description: Field<string>;
+    Facebook: Field<string>;
+    Twitter: Field<string>;
+    Linkedin: Field<string>;
+  };
+};
+
+const SpeakerInformation = (props: SpeakerInformationProps): JSX.Element => (
   <section className="section">
     <div className="section__content left__content">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
         <div className="col-span-1 md:col-span-1">
-          <img src="/data/media/img/speakers/mary-asada.jpeg" alt="Mary Asada" />
+          <Image field={props.fields?.Image} alt={props.fields?.name.value} />
           <div>
-            <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faFacebookF} />
-            <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faTwitter} />
-            <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faLinkedinIn} />
-            <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faInstagram} />
+            {/* TODO: To be turned into links */}
+            <a href={props.fields.Facebook.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faFacebookF} />
+            </a>
+            <a href={props.fields.Facebook.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faTwitter} />
+            </a>
+            <a href={props.fields.Facebook.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faLinkedinIn} />
+            </a>
+            <a href={props.fields.Facebook.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faInstagram} />
+            </a>
           </div>
           <div className="pt-5">
-            <strong>Position:</strong> Lorem Ipsum Dolor
+            <strong>Position:</strong> {props.fields.Position.value}
           </div>
           <div className="pt-5">
-            <strong>Company:</strong> Lorem Dolor Media
+            <strong>Company:</strong> {props.fields.Company.value}
           </div>
           <div className="pt-5">
-            <strong>Country:</strong> Canada
+            <strong>Country:</strong> {props.fields.Country.value}
           </div>
         </div>
         <div className="col-span-1 md:col-span-3 space-y-5">
+          {/* TODO: Add speaker type in content hub */}
           <h3 className="">SPEAKER</h3>
-          <h2 className="text-2xl md:text-3xl font-extrabold text-blue">Mary Asada</h2>
-          <p className="text-base">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-            invidunt ut labore et dolore magna aliquyam erat, sed.
-          </p>
-          <p className="text-sm">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-            sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-            amet. At vero eos et accusam et justo.
-          </p>
-          <ul className="text-sm pl-10">
-            <li>- Lorem ipsum dolor sit amet</li>
-            <li>- consetetur sadipscing elitr</li>
-            <li>- sed diam nonumy eirmod tempor</li>
-            <li>- invidunt ut labore et dolore</li>
-            <li>- magna aliquyam erat</li>
-            <li>- sed diam voluptua</li>
-          </ul>
+          <h2 className="text-2xl md:text-3xl font-extrabold text-blue">
+            {props.fields.name.value}
+          </h2>
+          <RichText field={props.fields.description} />
+          {/* TODO: Link sessions with speakers and show info here */}
           <div>
             <div>
               <strong>Talks</strong>
@@ -67,4 +75,5 @@ const SpeakerInformation = (): JSX.Element => (
     </div>
   </section>
 );
+
 export default SpeakerInformation;
