3188e1912624e4a922a7e72d5582d4cdec76d033
Minhaz Topaz <mint@sitecore.net>
2021-07-25 07:19:19 -0600

    speaker social field rename

--- Website/src/rendering/src/components/SpeakersGrid.tsx
+++ Website/src/rendering/src/components/SpeakersGrid.tsx
@@ -16,29 +16,25 @@ type SpeakersGridProps = ComponentProps & {
   };
 };
 
-const SpeakersGrid = (props: SpeakersGridProps): JSX.Element => {
-  console.log(props.fields);
-
-  return (
-    <div className="section__speakers__grid">
-      {props.fields.items &&
-        props.fields.items.map((speaker, index) => {
-          return (
-            <Link
-              key={index}
-              href={'/speakers/' + speaker.fields.Name.value.replace(/ /g, '')}
-              passHref
-            >
-              <a className="section__speakers__grid__speaker">
-                <Image field={speaker.fields.Image} alt={speaker.fields.Name} />
-                <Text className="speaker__name" tag="p" field={speaker.fields.Name}></Text>
-                <Text className="speaker__role" tag="p" field={speaker.fields.Role}></Text>
-              </a>
-            </Link>
-          );
-        })}
-    </div>
-  );
-};
+const SpeakersGrid = (props: SpeakersGridProps): JSX.Element => (
+  <div className="section__speakers__grid">
+    {props.fields.items &&
+      props.fields.items.map((speaker, index) => {
+        return (
+          <Link
+            key={index}
+            href={'/speakers/' + speaker.fields.Name.value.replace(/ /g, '')}
+            passHref
+          >
+            <a className="section__speakers__grid__speaker">
+              <Image field={speaker.fields.Image} alt={speaker.fields.Name} />
+              <Text className="speaker__name" tag="p" field={speaker.fields.Name}></Text>
+              <Text className="speaker__role" tag="p" field={speaker.fields.Role}></Text>
+            </a>
+          </Link>
+        );
+      })}
+  </div>
+);
 
 export default SpeakersGrid;
