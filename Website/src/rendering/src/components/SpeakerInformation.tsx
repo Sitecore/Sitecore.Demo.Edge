44df1730717b043a5b92dd1e9a875d7e4d33fee6
Minhaz Topaz <mint@sitecore.net>
2021-07-26 12:09:53 -0600

    speaker information datasource fix

--- Website/src/rendering/src/components/SpeakerInformation.tsx
+++ Website/src/rendering/src/components/SpeakerInformation.tsx
@@ -24,63 +24,60 @@ type SpeakerInformationProps = ComponentProps & {
   };
 };
 
-const SpeakerInformation = (props: SpeakerInformationProps): JSX.Element => {
-  console.log(props.fields.FacebookProfileLink);
-  return (
-    <section className="section">
-      <div className="section__content left__content">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
-          <div className="col-span-1 md:col-span-1">
-            <Image field={props.fields?.Image} alt={props.fields?.Name?.value} />
-            <div>
-              {/* TODO: To be turned into links */}
-              <a href={props.fields.FacebookProfileLink?.value}>
-                <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faFacebookF} />
-              </a>
-              <a href={props.fields.TwitterProfileLink?.value}>
-                <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faTwitter} />
-              </a>
-              <a href={props.fields.LinkedinProfileLink?.value}>
-                <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faLinkedinIn} />
-              </a>
-              <a href={props.fields.InstagramProfileLink?.value}>
-                <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faInstagram} />
-              </a>
-            </div>
-            <div className="pt-5">
-              <strong>Position:</strong> {props.fields.Position.value}
-            </div>
-            <div className="pt-5">
-              <strong>Company:</strong> {props.fields.Company.value}
-            </div>
-            <div className="pt-5">
-              <strong>Country:</strong> {props.fields.Country.value}
-            </div>
+const SpeakerInformation = (props: SpeakerInformationProps): JSX.Element => (
+  <section className="section">
+    <div className="section__content left__content">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
+        <div className="col-span-1 md:col-span-1">
+          <Image field={props.fields?.Image} alt={props.fields?.Name?.value} />
+          <div>
+            {/* TODO: To be turned into links */}
+            <a href={props.fields.FacebookProfileLink?.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faFacebookF} />
+            </a>
+            <a href={props.fields.TwitterProfileLink?.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faTwitter} />
+            </a>
+            <a href={props.fields.LinkedinProfileLink?.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faLinkedinIn} />
+            </a>
+            <a href={props.fields.InstagramProfileLink?.value}>
+              <FontAwesomeIcon className="icon h-4 m-2 inline text-blue" icon={faInstagram} />
+            </a>
           </div>
-          <div className="col-span-1 md:col-span-3 space-y-5">
-            {/* TODO: Add speaker type in content hub */}
-            <Text tag="h3" className="uppercase" field={props.fields.Role}></Text>
-            <Text
-              tag="h2"
-              className="text-2xl md:text-3xl font-extrabold text-blue"
-              field={props.fields.Name}
-            ></Text>
-            <RichText field={props.fields.Description} />
-            {/* TODO: Link sessions with speakers and show info here */}
+          <div className="pt-5">
+            <strong>Position:</strong> {props.fields.Position.value}
+          </div>
+          <div className="pt-5">
+            <strong>Company:</strong> {props.fields.Company.value}
+          </div>
+          <div className="pt-5">
+            <strong>Country:</strong> {props.fields.Country.value}
+          </div>
+        </div>
+        <div className="col-span-1 md:col-span-3 space-y-5">
+          {/* TODO: Add speaker type in content hub */}
+          <Text tag="h3" className="uppercase" field={props.fields.Role}></Text>
+          <Text
+            tag="h2"
+            className="text-2xl md:text-3xl font-extrabold text-blue"
+            field={props.fields.Name}
+          ></Text>
+          <RichText field={props.fields.Description} />
+          {/* TODO: Link sessions with speakers and show info here */}
+          <div>
             <div>
-              <div>
-                <strong>Talks</strong>
-              </div>
-              <div className="border border-gray p-5 my-5">
-                <p>Mon, 19th | 9:00 AM</p>
-                <p className="font-bold">10 Tips to get the most out of your routines</p>
-              </div>
+              <strong>Talks</strong>
+            </div>
+            <div className="border border-gray p-5 my-5">
+              <p>Mon, 19th | 9:00 AM</p>
+              <p className="font-bold">10 Tips to get the most out of your routines</p>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default SpeakerInformation;
