4db099eddfed6f7836ea57edd42f9f462a6841fd
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-26 11:54:50 -0400

    Added ThreeColumnCta component

---
+++ Website/src/rendering/src/components/ThreeColumnCta.tsx
@@ -0,0 +1,34 @@
+import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+type ThreeColumnCtaProps = ComponentProps & {
+  fields: {
+    leftHeading: Field<string>;
+    leftDescription: Field<string>;
+    middleHeading: Field<string>;
+    middleDescription: Field<string>;
+    rightHeading: Field<string>;
+    rightDescription: Field<string>;
+  };
+};
+
+const ThreeColumnCta = (props: ThreeColumnCtaProps): JSX.Element => (
+  <section className="section text-center bg-black py-10 hidden md:block">
+  <div className="container justify-between text-xl flex">
+    <div>    
+      <h2 className="text-blue-lightest text-5xl font-bold py-2"><Text field={props.fields.leftHeading} /></h2>
+      <h3 className="uppercase text-white font-bold"><Text field={props.fields.leftDescription} /></h3>
+    </div>
+    <div>
+      <h2 className="text-blue-lightest text-5xl font-bold py-2"><Text field={props.fields.middleHeading} /></h2>
+      <h3 className="uppercase text-white font-bold"><Text field={props.fields.middleDescription} /></h3>
+    </div>
+    <div>
+      <h2 className="text-blue-lightest text-5xl font-bold py-2"><Text field={props.fields.rightHeading} /></h2>
+      <h3 className="uppercase text-white font-bold"><Text field={props.fields.rightDescription} /></h3>
+    </div>
+  </div>
+</section>
+);
+
+export default ThreeColumnCta;
