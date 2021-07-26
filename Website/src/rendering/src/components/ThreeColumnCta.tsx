8aee6a18e1cc1f4e5168b054b3b2d03b32e50de9
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-26 12:04:29 -0400

    Update ThreeColumnCta.tsx

--- Website/src/rendering/src/components/ThreeColumnCta.tsx
+++ Website/src/rendering/src/components/ThreeColumnCta.tsx
@@ -14,21 +14,33 @@ type ThreeColumnCtaProps = ComponentProps & {
 
 const ThreeColumnCta = (props: ThreeColumnCtaProps): JSX.Element => (
   <section className="section text-center bg-black py-10 hidden md:block">
-  <div className="container justify-between text-xl flex">
-    <div>    
-      <h2 className="text-blue-lightest text-5xl font-bold py-2"><Text field={props.fields.leftHeading} /></h2>
-      <h3 className="uppercase text-white font-bold"><Text field={props.fields.leftDescription} /></h3>
+    <div className="container justify-between text-xl flex">
+      <div>
+        <h2 className="text-blue-lightest text-5xl font-bold py-2">
+          <Text field={props.fields.leftHeading} />
+        </h2>
+        <h3 className="uppercase text-white font-bold">
+          <Text field={props.fields.leftDescription} />
+        </h3>
+      </div>
+      <div>
+        <h2 className="text-blue-lightest text-5xl font-bold py-2">
+          <Text field={props.fields.middleHeading} />
+        </h2>
+        <h3 className="uppercase text-white font-bold">
+          <Text field={props.fields.middleDescription} />
+        </h3>
+      </div>
+      <div>
+        <h2 className="text-blue-lightest text-5xl font-bold py-2">
+          <Text field={props.fields.rightHeading} />
+        </h2>
+        <h3 className="uppercase text-white font-bold">
+          <Text field={props.fields.rightDescription} />
+        </h3>
+      </div>
     </div>
-    <div>
-      <h2 className="text-blue-lightest text-5xl font-bold py-2"><Text field={props.fields.middleHeading} /></h2>
-      <h3 className="uppercase text-white font-bold"><Text field={props.fields.middleDescription} /></h3>
-    </div>
-    <div>
-      <h2 className="text-blue-lightest text-5xl font-bold py-2"><Text field={props.fields.rightHeading} /></h2>
-      <h3 className="uppercase text-white font-bold"><Text field={props.fields.rightDescription} /></h3>
-    </div>
-  </div>
-</section>
+  </section>
 );
 
 export default ThreeColumnCta;
