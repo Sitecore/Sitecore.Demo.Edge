99e9b1aba725eefabe36887796fec3df647437a5
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-27 15:42:01 -0400

    Updated component, definition and story

--- Website/src/rendering/sitecore/definitions/components/ThreeColumnsSection.sitecore.ts
+++ Website/src/rendering/sitecore/definitions/components/ThreeColumnsSection.sitecore.ts
@@ -10,7 +10,19 @@ const ThreeColumnsSection = function (manifest: Manifest): void {
   manifest.addComponent({
     name: 'ThreeColumnsSection',
     icon: SitecoreIcon.DocumentTag,
-    fields: [{ name: 'heading', type: CommonFieldTypes.SingleLineText }],
+    fields: [
+      { name: 'Title', type: CommonFieldTypes.SingleLineText },
+      { name: 'Subtitle', type: CommonFieldTypes.SingleLineText },
+      { name: 'LeftLogo', type: CommonFieldTypes.SingleLineText },
+      { name: 'LeftTitle', type: CommonFieldTypes.SingleLineText },
+      { name: 'LeftLink', type: CommonFieldTypes.SingleLineText },
+      { name: 'MiddleLogo', type: CommonFieldTypes.SingleLineText },
+      { name: 'MiddleTitle', type: CommonFieldTypes.SingleLineText },
+      { name: 'MiddleLink', type: CommonFieldTypes.SingleLineText },
+      { name: 'RightLogo', type: CommonFieldTypes.SingleLineText },
+      { name: 'RightTitle', type: CommonFieldTypes.SingleLineText },
+      { name: 'RightLink', type: CommonFieldTypes.SingleLineText },
+    ],
     /*
     If the component implementation uses <Placeholder> or withPlaceholder to expose a placeholder,
     register it here, or components added to that placeholder will not be returned by Sitecore:
