549d2a5ce3d618ac1e23a503270b4d2c0718a18a
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-29 09:41:51 -0400

    Refactoring, added story

--- Website/src/rendering/src/components/ProductCategoriesGrid.tsx
+++ Website/src/rendering/src/components/ProductCategoriesGrid.tsx
@@ -1,76 +1,43 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { Text, Field, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
 
-import product1 from '../../data/media/img/products/team-sports.jpg';
-import product2 from '../../data/media/img/products/water-sports.jpg';
-import product3 from '../../data/media/img/products/motor-sports.jpg';
-import product4 from '../../data/media/img/products/hiking.jpg';
-import product5 from '../../data/media/img/products/fitness.jpg';
-import product6 from '../../data/media/img/products/yoga.jpg';
+export type Category = {
+  fields: {
+    Title: Field<string>;
+    Picture: ImageField;
+  };
+};
 
-const ProductCategoriesGrid = (): JSX.Element => (
-  <div className="section__products__grid">
-    <div className="section__products__grid__product">
-      <Link href="/shop/teamsports">
-        <a>
-          <figure>
-            <Image src={product1} alt="Category" width={350} height={250} />
-          </figure>
-          <h3 className="product__category">Team Sports</h3>
-        </a>
-      </Link>
+type ProductCategoriesProps = ComponentProps & {
+  fields: {
+    Title: Field<string>;
+    Subtitle: Field<string>;
+    Categories: Category[];
+  };
+};
+
+const ProductCategoriesGrid = (props: ProductCategoriesProps): JSX.Element => {
+  return (
+    <div className="section__products__grid">
+       {props.fields.Categories &&
+          props.fields.Categories.map((category, index) => (
+            <div className="section__products__grid__product">
+            <Link href="/shop/teamsports">
+              <a>
+                <figure>
+                  <Image src={category.fields.Picture.value?.src} alt="Category" width={350} height={250} />
+                </figure>
+                <h3 className="product__category">
+                  <Text field={category.fields.Title} />
+                </h3>
+              </a>
+            </Link>
+          </div>
+          ))}
     </div>
-    <div className="section__products__grid__product">
-      <Link href="/shop/swimming">
-        <a>
-          <figure>
-            <Image src={product2} alt="Category" width={350} height={250} />
-          </figure>
-          <h3 className="product__category">Swimming</h3>
-        </a>
-      </Link>
-    </div>
-    <div className="section__products__grid__product">
-      <Link href="/shop/motorsports">
-        <a>
-          <figure>
-            <Image src={product3} alt="Category" width={350} height={250} />
-          </figure>
-          <h3 className="product__category">Motor Sports</h3>
-        </a>
-      </Link>
-    </div>
-    <div className="section__products__grid__product">
-      <Link href="/shop/hiking">
-        <a>
-          <figure>
-            <Image src={product4} alt="Category" width={350} height={250} />
-          </figure>
-          <h3 className="product__category">Hike</h3>
-        </a>
-      </Link>
-    </div>
-    <div className="section__products__grid__product">
-      <Link href="/shop/fitness">
-        <a>
-          <figure>
-            <Image src={product5} alt="Category" width={350} height={250} />
-          </figure>
-          <h3 className="product__category">Fitness</h3>
-        </a>
-      </Link>
-    </div>
-    <div className="section__products__grid__product">
-      <Link href="/shop/yoga">
-        <a>
-          <figure>
-            <Image src={product6} alt="Category" width={350} height={250} />
-          </figure>
-          <h3 className="product__category">Yoga</h3>
-        </a>
-      </Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ProductCategoriesGrid;
---
+++ Website/src/rendering/src/stories/ProductCategoriesGrid.stories.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import ProductCategoriesGrid from '../components/ProductCategoriesGrid';
+import { Category } from '../components/ProductCategoriesGrid';
+
+export default {
+  title: 'Example/ProductCategoriesGrid',
+  component: ProductCategoriesGrid,
+} as ComponentMeta<typeof ProductCategoriesGrid>;
+
+const Template: ComponentStory<typeof ProductCategoriesGrid> = (args) => <ProductCategoriesGrid {...args} />;
+
+const category1 = {
+  Name: 'Item Name',
+  fields: {
+    Title: {
+      value: 'Team Sports',
+    },
+    Picture: {
+      value: {
+        src: '/assets/img/categories/team-sports.jpg',
+        alt: '',
+      },
+    },
+  },
+} as Category;
+
+const category2 = {
+  Name: 'Item Name',
+  fields: {
+    Title: {
+      value: 'Swimming',
+    },
+    Picture: {
+      value: {
+        src: '/assets/img/categories/water-sports.jpg',
+        alt: '',
+      },
+    },
+  },
+} as Category;
+
+const category3 = {
+  Name: 'Item Name',
+  fields: {
+    Title: {
+      value: 'Motor Sports',
+    },
+    Picture: {
+      value: {
+        src: '/assets/img/categories/motor-sports.jpg',
+        alt: '',
+      },
+    },
+  },
+} as Category;
+
+export const Default = Template.bind({});
+Default.args = {
+  fields: {
+    Title: {
+      value: 'FEATURED SPEAKERS',
+    },
+    Subtitle: {
+      value:
+        'Road-test the world’s most trusted sports and fitnessequipment–we’ll be welcoming 2,000 brands at this year’s PLAY! Summit.',
+    },
+    Categories: [category1, category2, category3],
+  },
+};
