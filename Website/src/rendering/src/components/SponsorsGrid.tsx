7b9a62d88220c89466123708efb007e2d2cbaa52
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-07-30 09:20:07 -0400

    Sponsor XM fields syncing with CH instance (#57)

--- Website/src/rendering/src/components/SponsorsGrid.tsx
+++ Website/src/rendering/src/components/SponsorsGrid.tsx
@@ -1,51 +1,44 @@
 import Link from 'next/link';
-import Image from 'next/image';
+import { Field, ImageField, Image } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
 
-const SponsorsGrid = (): JSX.Element => (
+type Sponsor = {
+  fields: {
+    Name: Field<string>;
+    Logo: ImageField;
+  };
+};
+
+type SponsorsProps = ComponentProps & {
+  fields: {
+    Title: Field<string>;
+    Subtitle: Field<string>;
+    Sponsors: Sponsor[];
+  };
+};
+
+const SponsorsGrid = (props: SponsorsProps): JSX.Element => (
   <div className="section__sponsors__grid">
-    <Link href="/sponsors/fitbit">
-      <a className="section__sponsors__grid__sponsor">
-        <div className="m-3">
-          <Image src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" width={180} height={80} />
-        </div>
-      </a>
-    </Link>
-    <Link href="/sponsors/sports">
-      <a className="section__sponsors__grid__sponsor">
-        <div className="m-3">
-          <Image src="/assets/img/sponsors-sports.svg" alt="Sports" width={180} height={80} />
-        </div>
-      </a>
-    </Link>
-    <Link href="/sponsors/fitbit">
-      <a className="section__sponsors__grid__sponsor">
-        <div className="m-3">
-          <Image src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" width={180} height={80} />
-        </div>
-      </a>
-    </Link>
-    <Link href="/sponsors/sports">
-      <a className="section__sponsors__grid__sponsor">
-        <div className="m-3">
-          <Image src="/assets/img/sponsors-sports.svg" alt="Sports" width={180} height={80} />
-        </div>
-      </a>
-    </Link>
-    <Link href="/sponsors/fitbit">
-      <a className="section__sponsors__grid__sponsor">
-        <div className="m-3">
-          <Image src="/assets/img/sponsors-fitbit.svg" alt="Fitbit" width={180} height={80} />
-        </div>
-      </a>
-    </Link>
-    <Link href="/sponsors/sports">
-      <a className="section__sponsors__grid__sponsor">
-        <div className="m-3">
-          <Image src="/assets/img/sponsors-sports.svg" alt="Sports" width={180} height={80} />
-        </div>
-      </a>
-    </Link>
+    {props &&
+      props.fields &&
+      props.fields.Sponsors &&
+      props.fields.Sponsors.map((sponsor, index) => (
+        <Link key={index} href="#">
+          <a className="section__sponsors__grid__sponsor">
+            <div className="m-3">
+              <Image
+                field={sponsor.fields.Logo}
+                alt={sponsor.fields.Name.value}
+                width={180}
+                height={80}
+              />
+            </div>
+          </a>
+        </Link>
+      ))}
   </div>
 );
 
+export type { Sponsor };
+export type { SponsorsProps };
 export default SponsorsGrid;
--- Website/src/rendering/src/stories/SponsorsGrid.stories.tsx
+++ Website/src/rendering/src/stories/SponsorsGrid.stories.tsx
@@ -1,14 +1,66 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import SponsorsGrid from '../components/SponsorsGrid';
+import SponsorsGrid, { Sponsor } from '../components/SponsorsGrid';
 
 export default {
   title: 'Example/SponsorsGrid',
   component: SponsorsGrid,
 } as ComponentMeta<typeof SponsorsGrid>;
 
-const Template: ComponentStory<typeof SponsorsGrid> = () => <SponsorsGrid />;
+const Template: ComponentStory<typeof SponsorsGrid> = (args) => <SponsorsGrid {...args} />;
+
+const sponsor1 = {
+  fields: {
+    Name: {
+      value: 'Fitbit',
+    },
+    Logo: {
+      value: {
+        src: '/assets/img/sponsors/sponsors-fitbit.svg',
+      },
+    },
+  },
+} as Sponsor;
+
+const sponsor2 = {
+  Name: 'Item Name',
+  fields: {
+    Name: {
+      value: 'Sports',
+    },
+    Logo: {
+      value: {
+        src: '/assets/img/sponsors/sponsors-sports.svg',
+      },
+    },
+  },
+} as Sponsor;
+
+const sponsor3 = {
+  Name: 'Item Name',
+  fields: {
+    Name: {
+      value: 'Fitbit',
+    },
+    Logo: {
+      value: {
+        src: '/assets/img/sponsors/sponsors-fitbit.svg',
+      },
+    },
+  },
+} as Sponsor;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  fields: {
+    Title: {
+      value: 'FEATURED SPEAKERS',
+    },
+    Subtitle: {
+      value:
+        'Road-test the world’s most trusted sports and fitnessequipment–we’ll be welcoming 2,000 brands at this year’s PLAY! Summit.',
+    },
+    Sponsors: [sponsor1, sponsor2, sponsor3],
+  },
+};
