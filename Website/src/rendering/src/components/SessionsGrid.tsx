2b4e6c0e7308a161429ac9180feeab73ca848f19
Minhaz Topaz <82103130+mintsitecore@users.noreply.github.com>
2021-08-04 09:35:17 -0600

    Session related pages components (#58)

--- Website/src/rendering/src/components/SessionsGrid.tsx
+++ Website/src/rendering/src/components/SessionsGrid.tsx
@@ -1,145 +1,110 @@
 import Link from 'next/link';
-import Image from 'next/image';
+import { Text, Field, ImageField, Image, DateField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
 
-import session1 from '../../data/media/img/sessions/session-1.jpg';
-import session2 from '../../data/media/img/sessions/session-2.jpg';
-import session3 from '../../data/media/img/sessions/session-3.jpg';
-import session4 from '../../data/media/img/sessions/session-4.jpg';
+type Speaker = {
+  fields: {
+    Name: Field<string>;
+    Role: Field<string>;
+  };
+};
 
-const SessionsGrid = (): JSX.Element => (
-  <section>
-    <div className="max-w-screen-2xl mx-auto box-border overflow-hidden">
-      <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session1} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">TRAIN SMARTER, NOT HARDER</div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
-        </div>
+type Session = {
+  fields: {
+    Name: Field<string>;
+    Image: ImageField;
+    Date: Field<string>;
+    Duration: Field<string>;
+    Speakers: Speaker[];
+  };
+};
 
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session2} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">FUEL FOR LIFE: NUTRITION 101</div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
-        </div>
+type SessionsGridProps = ComponentProps & {
+  fields: {
+    items: Session[];
+  };
+};
 
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session3} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">
-              MOUNTAIN BIKING: TALES FROM THE TRAIL
-            </div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session4} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">
-              7 MINDSET STRATEGIES TO RAISE YOUR GAME
-            </div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session4} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">
-              7 MINDSET STRATEGIES TO RAISE YOUR GAME
-            </div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session3} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">
-              MOUNTAIN BIKING: TALES FROM THE TRAIL
-            </div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session2} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">FUEL FOR LIFE: NUTRITION 101</div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="rounded overflow-hidden bg-white">
-          <Image src={session1} alt="Session" width={340} height={227} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-base mb-2 h-20">TRAIN SMARTER, NOT HARDER</div>
-            <p className="text-gray-700 text-xs pb-3">Mon, 24th | 11.15 AM –11:45 AM</p>
-            <p className="text-gray-700 text-xs pb-3">Duration: 30 minutes</p>
-            <p className="text-gray-700 text-xs">ANDRES VILLARES | Professional</p>
-          </div>
-          <div className="px-6 pt-4 pb-10">
-            <Link href="/tickets">
-              <a className="btn--main btn--main--round">Get Tickets</a>
-            </Link>
-          </div>
+const SessionsGrid = (props: SessionsGridProps): JSX.Element => {
+  console.log(props.fields);
+  return (
+    <section>
+      <div className="max-w-screen-2xl mx-auto box-border overflow-hidden">
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+          {props.fields.items &&
+            props.fields.items.map((session, index) => (
+              <div key={index} className="rounded overflow-hidden bg-white">
+                <Image
+                  field={session.fields.Image}
+                  alt={session.fields.Name}
+                  width={340}
+                  height={227}
+                />
+                <div className="px-6 py-4">
+                  <Text
+                    tag="div"
+                    className="font-bold text-base mb-2 h-18 uppercase"
+                    field={session.fields.Name}
+                  ></Text>
+                  <DateField
+                    tag="p"
+                    className="text-gray-700 text-xs pb-3"
+                    field={session.fields.Date}
+                    render={(date) =>
+                      date?.toLocaleDateString('en-US', {
+                        weekday: 'short',
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                      })
+                    }
+                  />
+                  <p className="text-gray-700 text-xs pb-3">
+                    <span>Start Time: </span>
+                    <DateField
+                      tag="span"
+                      field={session.fields.Date}
+                      render={(date) =>
+                        date?.toLocaleTimeString('en-US', {
+                          hour: '2-digit',
+                          minute: '2-digit',
+                        })
+                      }
+                    />
+                  </p>
+                  <p className="text-gray-700 text-xs pb-3">
+                    {/* TODO: change to field */}
+                    <span>Duration: {session.fields.Duration?.value?.replace('_', '.')}</span>
+                  </p>
+                  {session.fields.Speakers &&
+                    session.fields.Speakers.map((speaker, index) => (
+                      <p key={index}>
+                        <Text
+                          tag="span"
+                          className="text-gray-700 text-xs uppercase"
+                          field={speaker.fields.Name}
+                        ></Text>
+                        {' | '}
+                        <Text
+                          tag="span"
+                          className="text-gray-700 text-xs"
+                          field={speaker.fields.Role}
+                        ></Text>
+                      </p>
+                    ))}
+                </div>
+                <div className="px-6 pt-4 pb-10">
+                  <Link href="/tickets">
+                    <a className="btn--main btn--main--round">Get&nbsp;Tickets</a>
+                  </Link>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
+export type { Session, Speaker };
 export default SessionsGrid;
