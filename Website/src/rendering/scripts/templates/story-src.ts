27d57379c96229e0f17620ddf029bd40183ad544
Jeff L'Heureux <jel@sitecore.net>
2021-08-05 16:17:45 -0400

    Add Storybook story scaffolding.
    Remove manifest scaffolding.

---
+++ Website/src/rendering/scripts/templates/story-src.ts
@@ -0,0 +1,31 @@
+// DEMO TEAM CUSTOMIZATION - New file to generate a storybook story when scaffolding a component.
+/**
+ * Generates Storybook story boilerplate for a component under `src/stories`
+ * @param componentName - the component name
+ * @returns component story boilerplate as a string
+ */
+function generateStorySrc(componentName: string, componentPath: string): string {
+  const parentFolders = componentPath.replace(/\w/, '..');
+
+  return `import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import ${componentName} from '../${parentFolders}components/${componentPath}${componentName}';
+
+export default {
+  title: 'Components/${componentPath}${componentName}',
+  component: ${componentName},
+} as ComponentMeta<typeof ${componentName}>;
+
+const Template: ComponentStory<typeof ${componentName}> = (args) => <${componentName} {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  params: {
+    name: '${componentName}',
+  },
+};
+`;
+}
+
+export default generateStorySrc;
