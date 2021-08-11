93403fd9bb823a9d5ead54037f56b46572907ca1
Minhaz Topaz <mint@sitecore.net>
2021-08-11 00:13:13 -0600

    footer tv control

--- tv/pages/index.tsx
+++ tv/pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import banner1 from '../public/banner1.png';
 import banner2 from '../public/banner2.png';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { useState } from 'react';
 
 export default function Home() {
+  const [showMe, setShowMe] = useState(false);
+  function toggle() {
+    setShowMe(!showMe);
+    document.getElementsByClassName('footer')[0].classList.toggle('active');
+  }
+
   return (
     <div className="screen">
       <Head>
@@ -32,16 +40,23 @@ export default function Home() {
       </main>
 
       <footer className="footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className="logo">
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <div className="controller" onClick={toggle}>
+          <span className="controller-text">^</span>
+        </div>
+        <div className="controls">
+          <Link href="/room" passHref>
+            <a>Room </a>
+          </Link>
+          <Link href="/room" passHref>
+            <a>Sessions </a>
+          </Link>
+          <Link href="/room" passHref>
+            <a>Schedule</a>
+          </Link>
+          <Link href="/room" passHref>
+            <a>Speaker</a>
+          </Link>
+        </div>
       </footer>
     </div>
   );
