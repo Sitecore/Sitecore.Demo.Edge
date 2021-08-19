6c0b9f861b22c614588979d1092db21c3656f03b
Jeff L'Heureux <jel@sitecore.net>
2021-08-19 12:54:14 -0400

    FullImageSection

--- Website/src/rendering/src/stories/Sections/FullImageSection.stories.tsx
+++ Website/src/rendering/src/stories/Sections/FullImageSection.stories.tsx
@@ -10,8 +10,8 @@ export default {
 
 const Template: ComponentStory<typeof FullImageSection> = (args) => <FullImageSection {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Left = Template.bind({});
+Left.args = {
   fields: {
     cssClass: {
       value: 'section__full-image--offer',
@@ -23,17 +23,45 @@ Default.args = {
       },
     },
     content: {
-      value: '',
+      value:
+        'We’re partnering with over 2000 brands to offer PLAY! Summit attendees a <b>20% discount</b> on all online and in-person purchases made during this year’s event.',
     },
     position: {
       value: 'left',
     },
     subtitle: {
-      value: 'PLAY! Summit Exclusive Offer',
+      value: 'Do not miss',
     },
     title: {
+      value: 'PLAY! Summit Exclusive Offer',
+    },
+  },
+};
+
+export const Right = Template.bind({});
+Right.args = {
+  fields: {
+    cssClass: {
+      value: 'section__full-image--speaker',
+    },
+    callToActionLink: {
+      value: {
+        href: '/tickets',
+        text: 'Book your seat',
+      },
+    },
+    content: {
       value:
-        'We’re partnering with over 2000 brands to offer PLAY! Summit attendees a 20% discount on all online and in-person purchases made during this year’s event.',
+        'Join <b>Tour de France</b> champion Chris Williams for an exclusive Q & A in which he’ll discuss his professional journey and highlights from his career.',
+    },
+    position: {
+      value: 'right',
+    },
+    subtitle: {
+      value: 'Guest speaker',
+    },
+    title: {
+      value: 'Chris Williams',
     },
   },
 };
