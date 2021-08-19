efe386d7b7031a6094369d30442e4924ad93e4ba
Minhaz Topaz <mint@sitecore.net>
2021-08-19 15:51:32 -0600

    removing debug from pipeline and update template

--- Website/src/platform/Pipelines/DemoContextExtension.cs
+++ Website/src/platform/Pipelines/DemoContextExtension.cs
@@ -1,8 +1,4 @@
-﻿using System.Linq;
-using System.Web.Helpers;
-using Sitecore.ContentSearch.Utilities;
-using Sitecore.Data;
-using Sitecore.Data.Items;
+﻿using Sitecore.Data.Items;
 using Sitecore.JavaScriptServices.Configuration;
 using Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext;
 
@@ -17,31 +13,14 @@ namespace Sitecore.Demo.Edge.Website.Pipelines
 
         protected override void DoProcess(GetLayoutServiceContextArgs args, AppConfiguration application)
         {
-
-            Diagnostics.Log.Warn("Topaz RenderedItem " + (args.RenderedItem == null).ToString(), this);
-            Diagnostics.Log.Warn("Topaz RenderedItem " + args.RenderedItem.Name, this);
-            Diagnostics.Log.Warn("Topaz Database " + (args.RenderedItem.Database == null).ToString(), this);
-            Diagnostics.Log.Warn("Topaz Database " + args.RenderedItem.Database.Name, this);
-
-            Item eventItem = args.RenderedItem?.Database?.GetItem("/sitecore/content/CMP/Events/Play Summit");
+            Item eventItem = args?.RenderedItem?.Database?.GetItem("/sitecore/content/CMP/Events/Play Summit");
             
             if (eventItem == null) return;
 
-            Diagnostics.Log.Warn("Topaz eventItem " + (eventItem == null).ToString(), this);
-            Diagnostics.Log.Warn("Topaz eventItem " + eventItem.Name , this);
-
-            string s = string.Join(";", args.ContextData.Select(x => x.Key + " = " + x.Value).ToArray());
-            Diagnostics.Log.Warn("Topaz contextData " + s, this);
-
-            Diagnostics.Log.Warn("Topaz item " + (string.IsNullOrEmpty(eventItem.Fields["StartDate"].Value) ? "no date" : eventItem.Fields["StartDate"].Value), this);
-            Diagnostics.Log.Warn("Topaz item PageTitlePrefix " + (string.IsNullOrEmpty(eventItem.Fields["PageTitlePrefix"].Value) ? "no title" : eventItem.Fields["PageTitlePrefix"].Value), this);
-
             args.ContextData.Add("EventInfo", new
             {
-                name = "topaz",
-                EventId = eventItem?.ID.ToString(),
-                //StartDate = eventItem?.Fields["StartDate"].Value,
-                //TitlePrefix = eventItem?.Fields["PageTitlePrefix"].Value
+                StartDate = eventItem?.Fields["StartDate"]?.Value,
+                TitlePrefix = eventItem?.Fields["PageTitlePrefix"]?.Value
             });
         }
     }
