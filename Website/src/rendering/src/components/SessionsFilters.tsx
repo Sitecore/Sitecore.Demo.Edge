d17d15ff4ecdb9fd6701c714ab9900fecfe48754
Jeff L'Heureux <jel@sitecore.net>
2021-08-24 16:17:10 -0400

    SessionsFilters

--- Website/src/rendering/src/components/SessionsFilters.tsx
+++ Website/src/rendering/src/components/SessionsFilters.tsx
@@ -1,59 +1,69 @@
 const SessionsFilters = (): JSX.Element => (
   <section className="section">
-    <div className="section__content">
-      <h1 className="section__content__title section__content__title--light">Set your schedule</h1>
-      <div className="w-full">
-        <div className="px-10 flex flex-wrap overflow-hidden">
-          <div className="w-full overflow-hidden sm:w-full md:w-1/2">
-            <input
-              className="w-full md:w-3/5 rounded p-2 border border-white-dark text-sm font-medium"
-              type="text"
-              placeholder="Search sessions..."
-            />
-          </div>
-          <div className="w-full overflow-hidden sm:w-full md:w-1/2 flex justify-start md:justify-end">
-            <button
-              type="button"
-              className="hidden md:inline-flex justify-center w-40 border border-white-dark shadow-sm px-4 py-2 bg-white-light text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-              id="menu-button"
-              aria-expanded="true"
-              aria-haspopup="true"
-              aria-label="Results"
-            >
-              Results
-              <svg
-                className="-mr-1 ml-2 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+    <div className="section__content form item-grid sessions-filters">
+      <h2 className="section__content__title section__content__title--light">Explore sessions</h2>
+      <div className="filters-row">
+        <div className="filters-col">
+          <input type="text" placeholder="Search sessions..." />
         </div>
-
-        <div className="px-10 flex flex-wrap overflow-hidden pt-5">
-          <div className="w-full overflow-hidden sm:w-full md:w-1/2 flex justify-between md:justify-start">
-            <div className="pr-5 py-1 text-sm hidden md:block">
-              <p>214 sessions</p>
-            </div>
-            <button
-              type="button"
-              className="inline-flex justify-center w-30 border border-white-dark shadow-sm px-4 py-1 bg-white-light text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-              id="menu-button"
-              aria-expanded="true"
-              aria-haspopup="true"
-              aria-label="Chronological"
+        <div className="filters-col sort-by-col">
+          <button
+            type="button"
+            className="dropdown-filter sort-by-dropdown"
+            id="menu-button"
+            aria-expanded="true"
+            aria-haspopup="true"
+            aria-label="Chronological"
+          >
+            Chronological
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
             >
-              Chronological
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div className="filters-row">
+        <div className="filters-col sessions-count-col">
+          <div className="sessions-count">214 sessions</div>
+          <label className="checkbox-label space-available-checkbox">
+            <input type="checkbox" defaultChecked />
+            <span className="label-text">Space available</span>
+          </label>
+        </div>
+        <div className="filters-col pager-col">
+          <nav className="pager" aria-label="Pagination">
+            <span aria-current="page" className="pager-item active">
+              1
+            </span>
+            <a href="#" className="pager-item">
+              2
+            </a>
+            <a href="#" className="pager-item hiddeable">
+              3
+            </a>
+            <span className="pager-item ellipsis-item">...</span>
+            <a href="#" className="pager-item hiddeable">
+              8
+            </a>
+            <a href="#" className="pager-item">
+              9
+            </a>
+            <a href="#" className="pager-item">
+              10
+            </a>
+            <a href="#" className="pager-item next-page">
+              <span>Next</span>
               <svg
-                className="-mr-1 ml-2 h-5 w-5"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor"
@@ -61,88 +71,12 @@ const SessionsFilters = (): JSX.Element => (
               >
                 <path
                   fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                   clipRule="evenodd"
                 />
               </svg>
-            </button>
-            <label className="inline-flex items-center pl-5">
-              <input type="checkbox" className="form-checkbox" defaultChecked />
-              <span className="ml-2 text-sm">Space available</span>
-            </label>
-          </div>
-          <div className="w-full overflow-hidden sm:w-full md:w-1/2 flex pt-4 md:pt-0 justify-start md:justify-end">
-            <div className="flex items-center justify-between">
-              <div className="sm:flex-1 sm:flex sm:items-center sm:justify-between">
-                <div>
-                  <nav
-                    className="relative z-0 inline-flex rounded-md -space-x-px"
-                    aria-label="Pagination"
-                  >
-                    <a
-                      href="#"
-                      aria-current="page"
-                      className="z-10 bg-indigo-50 text-indigo-600 relative inline-flex items-center px-4 py-2 text-sm font-medium"
-                    >
-                      1
-                    </a>
-                    <a
-                      href="#"
-                      className="bg-white-light text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 text-sm font-medium"
-                    >
-                      2
-                    </a>
-                    <a
-                      href="#"
-                      className="bg-white-light text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 text-sm font-medium"
-                    >
-                      3
-                    </a>
-                    <span className="relative inline-flex items-center px-4 py-2 bg-white-light text-sm font-medium text-gray-700">
-                      ...
-                    </span>
-                    <a
-                      href="#"
-                      className="bg-white-light text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 text-sm font-medium"
-                    >
-                      8
-                    </a>
-                    <a
-                      href="#"
-                      className="bg-white-light text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 text-sm font-medium"
-                    >
-                      9
-                    </a>
-                    <a
-                      href="#"
-                      className="bg-white-light text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 text-sm font-medium"
-                    >
-                      10
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center px-2 py-2 rounded-r-md bg-white-light text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    >
-                      <span className="sr-only">Next</span>
-                      <svg
-                        className="h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </a>
-                  </nav>
-                </div>
-              </div>
-            </div>
-          </div>
+            </a>
+          </nav>
         </div>
       </div>
     </div>
