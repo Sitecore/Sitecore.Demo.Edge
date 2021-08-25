e0060a67823eece866c3457d62e34f8b84b9b134
Minhaz Topaz <mint@sitecore.net>
2021-08-25 11:00:26 -0600

    forms init

---
+++ Website/src/rendering/src/components/Form.tsx
@@ -0,0 +1,15 @@
+import { Form } from '@sitecore-jss/sitecore-jss-react-forms';
+import React from 'react';
+import { withRouter } from 'next/router';
+import { sitecoreApiHost, sitecoreApiKey } from '../temp/config';
+
+const JssNextForm = ({ fields, router }: any) => (
+  <Form
+    form={fields}
+    sitecoreApiHost={sitecoreApiHost}
+    sitecoreApiKey={sitecoreApiKey}
+    onRedirect={(url) => router.push(url)}
+  />
+);
+
+export default withRouter(JssNextForm);
