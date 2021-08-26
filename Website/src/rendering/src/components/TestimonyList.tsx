b902700f3fbf40411c9b4095c0f131e20a38eb59
Jeff L'Heureux <jel@sitecore.net>
2021-08-26 06:51:44 -0400

    Remove debug console.log

--- Website/src/rendering/src/components/TestimonyList.tsx
+++ Website/src/rendering/src/components/TestimonyList.tsx
@@ -17,46 +17,43 @@ type TestimonyListProps = ComponentProps & {
   };
 };
 
-const TestimonyList = (props: TestimonyListProps): JSX.Element => {
-  console.log(props);
-  return (
-    <section className="section section__testimonies">
-      <div className="section__content">
-        <Text
-          tag="h2"
-          className="section__content__title section__content__title--light"
-          field={props.fields.Title}
-        ></Text>
-        <Text
-          tag="div"
-          className="section__content__p section__content__p--light"
-          field={props.fields.Subtitle}
-        ></Text>
-        <div className="testimonies-list">
-          {props.fields.Companies &&
-            props.fields.Companies.map((testimony, index) => (
-              <div key={index} className="list-item">
-                <div className="provider-image">
-                  <Image
-                    field={testimony.fields.Logo}
-                    alt={testimony.fields.Provider.value}
-                    width={180}
-                    height={180}
-                  />
-                </div>
-                <div className="testimony">
-                  <RichText tag="div" field={testimony.fields.Testimony} />
-                  <div>
-                    - <Text tag="span" className="provider" field={testimony.fields.Provider} />
-                  </div>
+const TestimonyList = (props: TestimonyListProps): JSX.Element => (
+  <section className="section section__testimonies">
+    <div className="section__content">
+      <Text
+        tag="h2"
+        className="section__content__title section__content__title--light"
+        field={props.fields.Title}
+      ></Text>
+      <Text
+        tag="div"
+        className="section__content__p section__content__p--light"
+        field={props.fields.Subtitle}
+      ></Text>
+      <div className="testimonies-list">
+        {props.fields.Companies &&
+          props.fields.Companies.map((testimony, index) => (
+            <div key={index} className="list-item">
+              <div className="provider-image">
+                <Image
+                  field={testimony.fields.Logo}
+                  alt={testimony.fields.Provider.value}
+                  width={180}
+                  height={180}
+                />
+              </div>
+              <div className="testimony">
+                <RichText tag="div" field={testimony.fields.Testimony} />
+                <div>
+                  - <Text tag="span" className="provider" field={testimony.fields.Provider} />
                 </div>
               </div>
-            ))}
-        </div>
+            </div>
+          ))}
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export type { Testimony };
 export default TestimonyList;
