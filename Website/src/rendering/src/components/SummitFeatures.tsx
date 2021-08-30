fbcddba41503a04853f5bc0a182830cdb378a87b
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-08-30 09:58:34 -0400

    AB#6415 Summit Features (#96)

--- Website/src/rendering/src/components/SummitFeatures.tsx
+++ Website/src/rendering/src/components/SummitFeatures.tsx
@@ -1,49 +1,16 @@
-const SummitFeatures = (): JSX.Element => (
+import { Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+export type SummitFeaturesProps = ComponentProps & {
+  children: React.ReactNode;
+};
+
+const SummitFeatures = (props: SummitFeaturesProps): JSX.Element => (
   <div className="item-grid summit-features">
     <div className="grid-content">
-      <div className="grid-item">
-        <img src="/assets/img/aboutus/Play-Summit-Speaking-Hall2.jpg" alt="expo" />
-        <div className="item-details item-details-left">
-          <div className="item-title">The Expo</div>
-          <p className="item-description">
-            This year’s theme is ‘Raise Your Game’ –join us over two days for a global gathering of
-            brands, sports professionals,and innovators.
-          </p>
-        </div>
-      </div>
-      <div className="grid-item">
-        <img src="/assets/img/aboutus/fitbit-logo.png" alt="Speakers" />
-        <div className="item-details item-details-left">
-          <div className="item-title">Leading brands</div>
-          <p className="item-description">
-            Avail of exclusive discounts or road-test the latest sports equipment in our Exhibition
-            Hall, where you can explore 300 + leading sports brands.
-          </p>
-        </div>
-      </div>
-      <div className="grid-item">
-        <img
-          src="/assets/img/aboutus/crowds-people-at-cycling-exhibition-BKK44X.jpeg"
-          alt="Vendors"
-        />
-        <div className="item-details item-details-left">
-          <div className="item-title">Workshops</div>
-          <p className="item-description">
-            From transforming your mindset to improving your technique, learn practical tips from
-            our line-up of experts.
-          </p>
-        </div>
-      </div>
-      <div className="grid-item">
-        <img src="/assets/img/aboutus/P1220073.jpeg" alt="Vendors" />
-        <div className="item-details item-details-left">
-          <div className="item-title">Speakers</div>
-          <p className="item-description">
-            Sign up for a session and hear exclusive insights from 90+ globally recognized sports
-            stars, athletes, and fitness experts.
-          </p>
-        </div>
-      </div>
+      <Placeholder name="jss-summit-features-content" rendering={props.rendering} />
+
+      {props.children}
     </div>
   </div>
 );
