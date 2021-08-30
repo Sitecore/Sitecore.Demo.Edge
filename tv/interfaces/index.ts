371601a49691ee33bcfed15b02cef71907bff893
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-08-30 10:18:40 -0400

    Removed unused code

--- tv/interfaces/index.ts
+++ tv/interfaces/index.ts
@@ -1,15 +1,3 @@
-export interface Blog {
-  id: string;
-  blog_Title: string;
-  blog_Quote: string;
-  blog_Body: string;
-}
-
-export interface Timeslot {
-  id: string;
-  name: string;
-}
-
 export interface Params {
   id: string
 }
