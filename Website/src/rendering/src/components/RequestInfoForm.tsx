73ebcb0d2239990f1ae828e57fc371395d377ee6
Jeff L'Heureux <jel@sitecore.net>
2021-09-07 10:31:35 -0400

    Add thank you message on form submit

--- Website/src/rendering/src/components/RequestInfoForm.tsx
+++ Website/src/rendering/src/components/RequestInfoForm.tsx
@@ -1,63 +1,103 @@
+import { ComponentProps } from 'lib/component-props';
 import Link from 'next/link';
+import { Component } from 'react';
 
-const RequestInfoForm = (): JSX.Element => (
-  <div className="section__content form form--dark request-info-form">
-    <h2>Request Further Information</h2>
-    <div className="inline-fields">
-      <div className="floating-label-wrap">
-        <input type="text" placeholder="First Name" id="firstName" />
-        <label htmlFor="firstName">First Name</label>
+interface RequestInfoFormState {
+  isFormSubmitted: boolean;
+}
+
+class RequestInfoForm extends Component<ComponentProps, RequestInfoFormState> {
+  state: Readonly<RequestInfoFormState> = {
+    isFormSubmitted: false,
+  };
+
+  constructor(props: ComponentProps) {
+    super(props);
+
+    // Binding this keyword
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(): void {
+    this.setState({
+      isFormSubmitted: true,
+    });
+  }
+
+  render(): JSX.Element {
+    const form = (
+      <>
+        <div className="inline-fields">
+          <div className="floating-label-wrap">
+            <input type="text" placeholder="First Name" id="firstName" />
+            <label htmlFor="firstName">First Name</label>
+          </div>
+          <div className="floating-label-wrap">
+            <input type="text" placeholder="Last Name" id="lastName" />
+            <label htmlFor="lastName">Last Name</label>
+          </div>
+        </div>
+        <div className="inline-fields">
+          <div className="floating-label-wrap">
+            <input type="text" placeholder="Email" id="email" />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="floating-label-wrap">
+            <input type="text" placeholder="Company" id="company" />
+            <label htmlFor="company">Company</label>
+          </div>
+        </div>
+        <div className="inline-fields">
+          <div className="floating-label-wrap">
+            <input type="text" placeholder="Job Title" id="jobTitle" />
+            <label htmlFor="jobTitle">Job Title</label>
+          </div>
+          <div className="floating-label-wrap">
+            <input type="text" placeholder="Phone Number" id="phoneNumber" />
+            <label htmlFor="phoneNumber">Phone Number</label>
+          </div>
+        </div>
+        <label className="checkbox-label">
+          <input type="checkbox" />
+          <span className="label-text">If you wish to receive updates on the expo</span>
+        </label>
+        <label className="checkbox-label">
+          <input type="checkbox" />
+          <span className="label-text">
+            If you wish to receive updates from third party vendors and promotions
+          </span>
+        </label>
+        <div className="button-area">
+          <button className="btn--main btn--main--round" onClick={this.onSubmit}>
+            Submit
+          </button>
+        </div>
+        <div className="footnote">
+          <p>
+            Already have an account? <Link href="/account/login">Log in.</Link>
+          </p>
+          <p>
+            To find out more about how we are using this information you are giving up, please our{' '}
+            <Link href="/privacy">privacy statement</Link>
+          </p>
+        </div>
+      </>
+    );
+
+    const thankYouMessage = (
+      <div>
+        Thank you for your request for information. A PLAY! Summit representative will contact you
+        in the near future.
       </div>
-      <div className="floating-label-wrap">
-        <input type="text" placeholder="Last Name" id="lastName" />
-        <label htmlFor="lastName">Last Name</label>
+    );
+
+    return (
+      <div className="section__content form form--dark request-info-form">
+        <h2>Request Further Information</h2>
+        {this.state.isFormSubmitted ? thankYouMessage : form}
       </div>
-    </div>
-    <div className="inline-fields">
-      <div className="floating-label-wrap">
-        <input type="text" placeholder="Email" id="email" />
-        <label htmlFor="email">Email</label>
-      </div>
-      <div className="floating-label-wrap">
-        <input type="text" placeholder="Company" id="company" />
-        <label htmlFor="company">Company</label>
-      </div>
-    </div>
-    <div className="inline-fields">
-      <div className="floating-label-wrap">
-        <input type="text" placeholder="Job Title" id="jobTitle" />
-        <label htmlFor="jobTitle">Job Title</label>
-      </div>
-      <div className="floating-label-wrap">
-        <input type="text" placeholder="Phone Number" id="phoneNumber" />
-        <label htmlFor="phoneNumber">Phone Number</label>
-      </div>
-    </div>
-    <label className="checkbox-label">
-      <input type="checkbox" />
-      <span className="label-text">If you wish to receive updates on the expo</span>
-    </label>
-    <label className="checkbox-label">
-      <input type="checkbox" />
-      <span className="label-text">
-        If you wish to receive updates from third party vendors and promotions
-      </span>
-    </label>
-    <div className="button-area">
-      <Link href="/tickets">
-        <a className="btn--main btn--main--round">Submit</a>
-      </Link>
-    </div>
-    <div className="footnote">
-      <p>
-        Already have an account? <Link href="/account/login">Log in.</Link>
-      </p>
-      <p>
-        To find out more about how we are using this information you are giving up, please our{' '}
-        <Link href="/privacy">privacy statement</Link>
-      </p>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default RequestInfoForm;
