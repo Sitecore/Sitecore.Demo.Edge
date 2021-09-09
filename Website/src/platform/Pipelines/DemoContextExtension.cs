f5ec164420ef0c14391301007b66fd4929d32768
Minhaz Topaz <mint@sitecore.net>
2021-09-09 11:56:22 -0600

    moved hardcoded values to config

--- Website/src/platform/Pipelines/DemoContextExtension.cs
+++ Website/src/platform/Pipelines/DemoContextExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
 using Sitecore.JavaScriptServices.Configuration;
 using Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext;
 
@@ -7,20 +8,35 @@ namespace Sitecore.Demo.Edge.Website.Pipelines
     public class DemoContextExtension : Sitecore.JavaScriptServices.ViewEngine.LayoutService.Pipelines.
         GetLayoutServiceContext.JssGetLayoutServiceContextProcessor
     {
+        private string configItemPath;
+        public string ConfigItemPath { get { return configItemPath; } set { configItemPath = value; } }
+        
+        private string startDateFieldName;
+        public string StartDateFieldName { get { return startDateFieldName; } set { startDateFieldName = value; } }
+        
+        private string pageTitleFIeldName;
+        public string PageTitleFIeldName { get { return pageTitleFIeldName; } set { pageTitleFIeldName = value; } }
+
         public DemoContextExtension(IConfigurationResolver configurationResolver) : base(configurationResolver)
         {
         }
 
         protected override void DoProcess(GetLayoutServiceContextArgs args, AppConfiguration application)
         {
-            Item eventItem = args?.RenderedItem?.Database?.GetItem("/sitecore/content/EdgeWebsite/Config");
+            Assert.ArgumentNotNull(args, "args");
+            if (!string.IsNullOrEmpty(configItemPath) || !string.IsNullOrEmpty(startDateFieldName) || !string.IsNullOrEmpty(pageTitleFIeldName))
+            {
+                Assert.IsNotNull(configItemPath, "No config item path or field names in parameter");
+            }
+
+            Item eventItem = args?.RenderedItem?.Database?.GetItem(configItemPath);
             
             if (eventItem == null) return;
 
             args.ContextData.Add("EventInfo", new
             {
-                StartDate = eventItem?.Fields["StartDate"]?.Value,
-                TitlePrefix = eventItem?.Fields["PageTitle"]?.Value
+                StartDate = eventItem?.Fields[startDateFieldName]?.Value,
+                TitlePrefix = eventItem?.Fields[pageTitleFIeldName]?.Value
             });
         }
     }
