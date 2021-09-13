dd75cbd00ec224cd00f515b49aa3675943d499a4
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-09-13 09:49:32 +0300

    Fixed loading font in Experience Editor

--- Website/src/rendering/src/Layout.tsx
+++ Website/src/rendering/src/Layout.tsx
@@ -44,12 +44,16 @@ const Layout = ({ context }: LayoutProps): JSX.Element => {
   }
   // END CUSTOMIZATION
 
-  return (
+  return ( 
     <>
       <Head>
         {/*   // DEMO TEAM CUSTOMIZATION - Use Event name as the page title from context */}
         <title>{pageTitle}</title>
         <link rel="icon" href={`${publicUrl}/favicon.ico`} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       {/* DEMO TEAM CUSTOMIZATION - Remove VisitorIdentification and Navigation */}
