ac3df6461b45595fd7d9761ffd727961399e6a40
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-09-13 12:59:48 +0300

    Formatting

--- Website/src/rendering/src/stories/SamplePage.stories.tsx
+++ Website/src/rendering/src/stories/SamplePage.stories.tsx
@@ -4,10 +4,10 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Header from '../components/Header';
 import { HeaderProps } from '../components/Header';
 import HeroSection, { HeroProps } from '../components/HeroSection';
-import MainNavigation, {MainNavigationProps} from '../components/MainNavigation';
+import MainNavigation, { MainNavigationProps } from '../components/MainNavigation';
 import ThreeColumnsSection, { ThreeColumnsSectionProps } from '../components/ThreeColumnsSection';
 import SponsorsGrid, { Sponsor, SponsorsProps } from '../components/SponsorsGrid';
-import Footer, {FooterProps} from '../components/Footer';
+import Footer, { FooterProps } from '../components/Footer';
 
 export default {
   title: 'Pages/Home',
@@ -192,7 +192,7 @@ const Template: ComponentStory<typeof HeroSection> = () => {
     <>
       <header>
         <Header {...headerProps} />
-        <MainNavigation {...mainNavigationArgs}/>
+        <MainNavigation {...mainNavigationArgs} />
       </header>
       <main>
         <HeroSection {...heroProps} />
