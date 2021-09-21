537439aac12979da46dd56c4710ab43252479d51
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-09-21 17:20:58 +0300

    Update TicketTiers.tsx

--- kiosk/components/TicketTiers.tsx
+++ kiosk/components/TicketTiers.tsx
@@ -3,7 +3,7 @@ import { TICKETS } from '../models/mock-tickets';
 
 const TicketTiers = (): JSX.Element => {
   return (
-    <div className="min-h-full bg-black flex justify-center items-center py-20">
+    <div className="min-h-full bg-black flex justify-center items-center py-10">
       <div className="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
         <Ticket ticket={TICKETS[0]} />
         <Ticket ticket={TICKETS[1]} />
