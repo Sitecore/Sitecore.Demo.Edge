25e9becf4a52ffa07d83254c0debdd7b82857701
Scott Mulligan <scm@sitecore.net>
2021-09-29 18:23:38 -0400

    Working Init Job for replacing the DAM URL in all fields

--- docker/build/init/Jobs/UpdateDamUri.cs
+++ docker/build/init/Jobs/UpdateDamUri.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using Newtonsoft.Json;
 using Sitecore.Demo.Init.Model;
-using Sitecore.Demo.Init.Extensions;
 using Microsoft.Extensions.Logging;
-using System.Net;
-using System.Collections.Generic;
 using System.Linq;
 using HtmlAgilityPack;
-using YamlDotNet.RepresentationModel;
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
 
 namespace Sitecore.Demo.Init.Jobs
 {
@@ -26,112 +16,107 @@ namespace Sitecore.Demo.Init.Jobs
 		}
 
 		public static string damUrl = Environment.GetEnvironmentVariable("DAM_CONTENTHUB");
+		public static string rootpath = @"C:\app\src\items\content\EdgeWebsite";
 
-		public async Task Run()
+	   	public async Task Run()
 		{
-			if (this.IsCompleted())
-			{
-				Log.LogWarning($"{this.GetType().Name} is already complete, it will not execute this time");
-				return;
-			}
-			
 			if (string.IsNullOrWhiteSpace(damUrl))
 			{
 				Log.LogWarning($"{this.GetType().Name} has been skipped, it does not execute if the DAM_URL variable is not passed to the Init container");
-			 	return;
+				return;
 			}
 
-			Log.LogInformation($"UpdateDamUri() started");
+			Log.LogInformation($"{this.GetType().Name} started");
 
-		  	UpdateValues();
+			if (!Directory.Exists(rootpath))
+			{
+				Log.LogWarning($"{this.GetType().Name} did not complete - could not find root path");
+				return;
+			}
 
-		  	Log.LogInformation("UpdateDamUri() complete");
+			var files = Directory.EnumerateFiles(rootpath, "*.yml", SearchOption.AllDirectories);	
+			foreach (var filepath in files)
+			{
+				UpdateYaml(filepath);
+			}
+
+			Log.LogInformation($"{this.GetType().Name} complete");
 			await Complete();
 		}
 
-		private void UpdateValues()
+		private void UpdateYaml(string filepath)
 		{
-		  var filepath = @"C:\app\src\items\content\EdgeWebsite\home.yml";
-		  string text = File.ReadAllText(filepath);
+			var deserializer = new YamlDotNet.Serialization.Deserializer();		
+			YamlItemModel yamlObjectUpdated = new YamlItemModel();
 
-		  using (var stream = File.OpenRead(filepath))
-		  {
-			 using (var reader = new StreamReader(stream))
-			 {
-				var deserializer = new Deserializer();
-				var yamlKvp = deserializer.Deserialize<Dictionary<string, object>>(reader);
+			using (var reader = new StreamReader(filepath)) {
+				YamlItemModel yamlObject = deserializer.Deserialize<YamlItemModel>(reader);
+				yamlObjectUpdated = UpdateValues(yamlObject);
+			}
+			
+			WriteYaml(yamlObjectUpdated, filepath);
+		}
 
-				foreach (var kvp in yamlKvp)
+		private void WriteYaml(YamlItemModel yamlObject, string filepath)
+		{
+			using (var writer = new StreamWriter(filepath))
+			{
+				var serializer = new YamlDotNet.Serialization.Serializer();
+				serializer.Serialize(writer, yamlObject);
+			}
+		}
+
+		private YamlItemModel UpdateValues(YamlItemModel yamlObject)
+		{
+			if (yamlObject != null & yamlObject?.SharedFields != null)
+			{
+				foreach (var sharedField in yamlObject?.SharedFields)
 				{
-				    if (kvp.Key == "Languages")
-				    {
-					   foreach (Dictionary<object, object> test in (kvp.Value as List<object>))
-					   {
-						  foreach (var test2 in test)
-						  {
-							 if ((test2.Key as string) == "Versions")
-							 {
-								foreach (Dictionary<object, object> test3 in (test2.Value as List<object>))
-								{
-								    foreach (var test4 in test3)
-								    {
-									   if ((test4.Key as string) == "Fields")
-									   {
-										  foreach (Dictionary<object, object> test5 in (test4.Value as List<object>))
-										  {
-											 object tryGetValue;
-											 test5.TryGetValue("Hint", out tryGetValue);
-											 if (!tryGetValue.ToString().StartsWith("__"))
-											 {
-												foreach (var test6 in test5)
-												{
-												    if (test6.Key.ToString() == "Value")
-												    {
-													   text = text.Replace(test6.Value.ToString(), "100" + test6.Value.ToString() + "100");
-												    }
-												}
-											 }
-										  }
-									   }
-								    }
-								}
-							 }
-						  }
-					   }
-				    }
+					if ((bool)(sharedField?.Hint.StartsWith("__")))
+						continue;
+					else if (sharedField?.Value != null && (bool)(sharedField?.Value.Contains("stylelabs-content-id")))
+						sharedField.Value = GetUpdatedDamHost(sharedField.Value);	
 				}
+			}
+			
+			if (yamlObject != null & yamlObject?.Languages != null)
+			{
+				foreach (var language in yamlObject?.Languages)
+				{
+					if (language?.Fields != null)
+					{
+						foreach (var field in language?.Fields)
+						{
+							if ((bool)(field?.Hint.StartsWith("__")))
+								continue;
+							else if (field?.Value != null && (bool)(field?.Value.Contains("stylelabs-content-id")))
+								field.Value = GetUpdatedDamHost(field.Value);
+						}
+					}
+					if (language?.Versions != null)
+					{
+						foreach (var version in language?.Versions)
+						{
+							if (version?.Fields != null)
+							{
+								foreach (var field in version?.Fields)
+								{
+									if ((bool)(field?.Hint.StartsWith("__")))
+										continue;
+									else if (field?.Value != null && (bool)(field?.Value.Contains("stylelabs-content-id")))
+										field.Value = GetUpdatedDamHost(field.Value);
+								}
+							}
+						}
+					}
+				}
+			}
 
-			 }
-		  }
+			return yamlObject;
+		}
 
-		  File.WriteAllText(filepath, text);
-
-	   }
-
-        private string ReadItemField(string hostCM, string token, string itemId)
-        {
-            try
-            {
-                var cookieClient = new CookieWebClient();
-                cookieClient.Encoding = Encoding.UTF8;
-                cookieClient.Headers.Add("token", token);
-                cookieClient.Headers.Add("Content-Type", "application/json");
-
-                var stringResponse = cookieClient.DownloadString(
-                    new Uri(hostCM + $"/sitecore/api/ssc/item/{itemId}?database=master&fields=image"));
-
-                var jsonDictionarySet = JsonConvert.DeserializeObject<Dictionary<String, String>>(stringResponse);
-                return jsonDictionarySet?.FirstOrDefault(x => x.Key == "image").Value;
-            }
-            catch (Exception ex)
-            {
-                Log.LogError($"Failed to read item field, itemId: {itemId}", ex);
-                return null;
-            }
-        }
-
-        private static string GetUpdatedDamHost(string existingFieldValue)
-		{	
+        private string GetUpdatedDamHost(string existingFieldValue)
+		{
 			if (string.IsNullOrWhiteSpace(existingFieldValue) || !existingFieldValue.Contains("stylelabs-content-id"))
 				return string.Empty;	
 
@@ -158,25 +143,5 @@ namespace Sitecore.Demo.Init.Jobs
 
 			return existingFieldValue?.Replace(imageSrcHost, damHost);
 		}
-
-		private void UpdateItemField(string hostCM, string token, string itemId, string escapedUpdatedDamHost)
-		{
-            try
-            {
-                var cookieClient = new CookieWebClient();
-                cookieClient.Encoding = System.Text.Encoding.UTF8;
-                cookieClient.Headers.Add("token", token);
-                cookieClient.Headers.Add("Content-Type", "application/json");
-
-                cookieClient.UploadDataAsync(
-                    new Uri(hostCM + $"/sitecore/api/ssc/item/{itemId}?database=master"),
-                    "PATCH",
-                    System.Text.Encoding.UTF8.GetBytes($"{{\"image\": \"{escapedUpdatedDamHost}\" }}"));
-            }
-            catch (Exception ex)
-            {
-				Log.LogError($"Failed to update item field, itemId: {itemId}", ex);
-            }
-        }
 	}
 }
