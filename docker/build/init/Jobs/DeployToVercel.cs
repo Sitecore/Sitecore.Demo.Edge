cdea95ed461799398cea7e6327c953b5d4f5c35a
Jeff L'Heureux <jel@sitecore.net>
2021-10-06 12:55:46 -0400

    Simplify Vercel website URL for kiosk

--- docker/build/init/Jobs/DeployToVercel.cs
+++ docker/build/init/Jobs/DeployToVercel.cs
@@ -75,16 +75,9 @@ namespace Sitecore.Demo.Init.Jobs
                 return;
             }
 
-            var websiteUrl = Environment.GetEnvironmentVariable("WEBSITE_URL");
-            if (string.IsNullOrEmpty(websiteUrl))
-            {
-                Log.LogWarning($"{this.GetType().Name} will not execute this time, WEBSITE_URL is not configured");
-                return;
-            }
-
             DeployTv(ns, contentHubApiKey, token, scope);
             DeployWebsite(ns, cdpClientKey, cdpApiTargetEndpoint, cdpProxyUrl, token, scope);
-            DeployKiosk(ns, cdpClientKey, cdpApiTargetEndpoint, websiteUrl, token, scope);
+            DeployKiosk(ns, cdpClientKey, cdpApiTargetEndpoint, token, scope);
 
             await Complete();
         }
@@ -157,7 +150,7 @@ namespace Sitecore.Demo.Init.Jobs
             cmd.Run($"vercel domains add {ns}-website.sitecoredemo.com --token {token} --scope {scope}");
         }
 
-        private static void DeployKiosk(string ns, string cdpClientKey, string cdpApiTargetEndpoint, string websiteUrl, string token, string scope)
+        private static void DeployKiosk(string ns, string cdpClientKey, string cdpApiTargetEndpoint, string token, string scope)
         {
             var sourceDirectory = "C:\\app\\kiosk";
             var targetDirectory = $"C:\\app\\{ns}-kiosk";
@@ -178,7 +171,7 @@ namespace Sitecore.Demo.Init.Jobs
             cmd.Run($"echo | set /p=\"{cdpClientKey}\" | vercel env add NEXT_PUBLIC_CDP_CLIENT_KEY production --token {token} --scope {scope}");
             cmd.Run(
                 $"echo | set /p=\"{cdpApiTargetEndpoint}\" | vercel env add NEXT_PUBLIC_CDP_API_TARGET_ENDPOINT production --token {token} --scope {scope}");
-            cmd.Run($"echo | set /p=\"{websiteUrl}\" | vercel env add NEXT_PUBLIC_WEBSITE_URL production --token {token} --scope {scope}");
+            cmd.Run($"echo | set /p=\"https://{ns}-website.sitecoredemo.com\" | vercel env add NEXT_PUBLIC_WEBSITE_URL production --token {token} --scope {scope}");
 
             // Deploy project files
             cmd.Run($"vercel --confirm --debug --prod --no-clipboard --token {token} --scope {scope}");
