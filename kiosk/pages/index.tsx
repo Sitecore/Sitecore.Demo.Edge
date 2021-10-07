c6f68b4e9007f21d6db1bd2a7ed35f1c7de833b0
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-10-07 13:47:29 +0300

    Update index.tsx

--- kiosk/pages/index.tsx
+++ kiosk/pages/index.tsx
@@ -1,10 +1,5 @@
-import HeroSection from '../components/HeroSection';
-
 export default function Home() {
   return (
-    <div
-      className="h-screen w-screen"
-      onClick={() => (window.location.href = '/start')}
-    ></div>
+    <div className="h-screen w-screen" onClick={() => (window.location.href = '/start')}></div>
   );
 }
