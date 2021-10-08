bd53439e32cc9a1aea938c8b15ae51a6e5438203
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-10-08 14:27:19 +0300

    Update contentHubImageLoader.ts

--- tv/utilities/contentHubImageLoader.ts
+++ tv/utilities/contentHubImageLoader.ts
@@ -5,5 +5,5 @@ interface Props {
 export const contentHubImageLoader = ({ src }: Props) => {
   const contentHubUrl = 'https://playsummit.sitecoresandbox.cloud';
 
-  return `${contentHubUrl}/api/public/content/${src}`;
+  return `${contentHubUrl}/api/public/content/${src}&t=web`;
 };
