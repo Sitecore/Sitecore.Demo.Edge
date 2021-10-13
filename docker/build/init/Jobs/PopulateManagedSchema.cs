585b3a7ea386654c64254cc0f586f45347f61410
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-10-13 15:51:23 +0300

    Update PopulateManagedSchema.cs

--- docker/build/init/Jobs/PopulateManagedSchema.cs
+++ docker/build/init/Jobs/PopulateManagedSchema.cs
@@ -35,8 +35,13 @@ namespace Sitecore.Demo.Init.Jobs
                 return;
             }
 
-            var cm = Environment.GetEnvironmentVariable("HOST_CM");
-            var id = Environment.GetEnvironmentVariable("HOST_ID");
+            var cm = Environment.GetEnvironmentVariable("PUBLIC_HOST_CM");
+            var id = Environment.GetEnvironmentVariable("PUBLIC_HOST_ID");
+            if (string.IsNullOrEmpty(cm) || string.IsNullOrEmpty(id))
+            {
+                Log.LogWarning($"{this.GetType().Name} will not execute, PUBLIC_HOST_CM and PUBLIC_HOST_ID are not configured");
+                return;
+            }
 
             var cmd = new WindowsCommandLine("C:\\app");
             cmd.Run($"dotnet sitecore login --client-credentials true --auth {id} --cm {cm} --allow-write true --client-id \"Demo_Automation\" --client-secret \"{token}\" -t");
