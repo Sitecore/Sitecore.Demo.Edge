b22221b0704f9c70a7117f93da1d83174b28d5c4
Minhaz Topaz <mint@sitecore.net>
2021-10-19 23:29:57 -0600

    hacky way working

--- tv/pages/schedule.tsx
+++ tv/pages/schedule.tsx
@@ -6,7 +6,7 @@ type ScheduleProps = {
   schedule: Session[];
 };
 
-function groupBy(list: any[], keyGetter: (arg0: any) => any) {
+function groupBy(list: unknown[], keyGetter: (arg0: unknown) => unknown) {
   const map = new Map();
   list.forEach((item) => {
     const key = keyGetter(item);
@@ -21,56 +21,30 @@ function groupBy(list: any[], keyGetter: (arg0: any) => any) {
 }
 
 const Schedule = (props: ScheduleProps): JSX.Element => {
-  console.log(props.schedule);
-
-  const uniques = props.schedule
-    .map((item) => item.timeslot)
-    .filter((value, index, self) => self.indexOf(value) === index);
-  console.log(uniques);
-
   const groupedSchedule = groupBy(props.schedule, (ts) => ts.timeslot);
   console.log(groupedSchedule);
 
+  const tableData: string[] = [];
+  groupedSchedule.forEach(function (item) {
+    let htmlBuilder = '';
+    console.log('------------foreach-----------');
+    console.log(item);
+    htmlBuilder = '<div class="bg-blue text-white p-4">' + item[0].timeslot + '</div>';
+
+    item.forEach((element: Session) => {
+      htmlBuilder += '<div class="bg-gray p-2 flex-1">' + element.name + '</div>';
+    });
+
+    tableData.push(htmlBuilder);
+  });
+
+  console.log(tableData);
+
   return (
-    <div>
-      {/* <div>
-        {props.days.map((day, index) => (
-          <div key={index}>
-            {day.name}
-            <br />
-            {day.timeslots.map((ts, ind) => (
-              <div key={ind}>{ts.taxonomyLabel}</div>
-            ))}
-          </div>
-        ))}
-      </div> */}
-      <div>
-        <table>
-          {/* {groupedSchedule.forEach(function (key) {
-            <tr>
-              <td>{groupedSchedule.get(key)}</td>
-            </tr>;
-          })} */}
-
-          {/* {Object.keys(groupedSchedule).forEach((key) => {
-            return <div>{key}</div>;
-
-            groupedSchedule.get(key).forEach((object: Session) => {
-              console.log(object);
-            });
-          })} */}
-
-          {/* {groupedSchedule.forEach((row) => (
-            <div>
-              <div> {row.key}</div>
-            </div>
-          ))} */}
-
-          {/* {props.schedule.map((session, index) => {
-            return <div key={index}>{session.timeslot + ' ' + session.name}</div>;
-          })} */}
-        </table>
-      </div>
+    <div className="flex flex-col space-y-2">
+      {tableData.map((row, index) => {
+        return <div className="flex gap-4" key={index} dangerouslySetInnerHTML={{ __html: row }} />;
+      })}
     </div>
   );
 };
