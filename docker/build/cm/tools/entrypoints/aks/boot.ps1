9390dbc5dab429872f20ef842a42a88f31305c62
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-10-25 14:21:08 +0300

    Update boot.ps1

--- docker/build/cm/tools/entrypoints/aks/boot.ps1
+++ docker/build/cm/tools/entrypoints/aks/boot.ps1
@@ -11,12 +11,7 @@ Write-Host "$(Get-Date -Format $timeFormat): ENTRYPOINT: '$JobName' starting..."
 
 Start-Job -Name $JobName -ArgumentList $JobParameters -ScriptBlock {
     param([hashtable]$params)
-
     & "C:\tools\scripts\Git-Sync.ps1" @params
-
 }
 
-# Test
-& "C:\tools\scripts\Git-Sync.ps1" @JobParameters
-
-#& "C:\LogMonitor\LogMonitor.exe" "powershell" "C:\Run-W3SVCService.ps1" 
+& "C:\LogMonitor\LogMonitor.exe" "powershell" "C:\Run-W3SVCService.ps1" 
