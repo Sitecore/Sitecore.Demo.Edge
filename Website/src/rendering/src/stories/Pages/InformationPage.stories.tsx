27fc582592776a17387002312cda7821d5482442
Jeff L'Heureux <jel@sitecore.net>
2021-10-25 09:47:33 -0400

    Markup for the VendorInformation and SessionList components

--- Website/src/rendering/src/stories/Pages/InformationPage.stories.tsx
+++ Website/src/rendering/src/stories/Pages/InformationPage.stories.tsx
@@ -64,17 +64,6 @@ const informationPageHeroProps = {
 
 const vendorInformationProps = {
   fields: {
-    Name: {
-      value: 'Fitbit',
-    },
-    Level: {
-      value: 'Featured',
-    },
-    Logo: {
-      value: {
-        src: '/assets/img/aboutus/fitbit-logo.png',
-      },
-    },
     Description: {
       value:
         '<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.&nbsp;</p><p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. At vero eos et accusam et justo.</p><ul><li>Lorem ipsum dolor sit amet</li><li>consetetur sadipscing elitr</li><li>sed diam nonumy eirmod tempor</li><li>invidunt ut labore et dolore</li><li>magna aliquyam erat</li><li>sed diam voluptua</li></ul><p>ad fas dasd asasdf asd fasd fasd fas dfasd f sdfasdfda sd as sdgf sdfg sdfg sdfbghtyurty urty urtyu rtyur tasdasqwqwrt wert wert wert wert sdfg sgd. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. At vero eos et accusam et justo.</p>',
@@ -108,7 +97,6 @@ const Template: ComponentStory<typeof InformationPageHero> = () => {
       </header>
       <main>
         <InformationPageHero {...informationPageHeroProps} />
-        <TicketGrid />
         <VendorInformation {...vendorInformationProps} />
       </main>
       <footer>
