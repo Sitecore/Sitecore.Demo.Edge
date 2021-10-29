25aab8669b1c3d76cc8daec2558eb588b78495e2
Minhaz Topaz <mint@sitecore.net>
2021-10-29 12:07:06 -0600

    cleanup to remove unused code

--- Website/src/platform/Pipelines/DemoCmpFieldMapping.cs
+++ Website/src/platform/Pipelines/DemoCmpFieldMapping.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using HtmlAgilityPack;
 using Sitecore.Abstractions;
 using Sitecore.Connector.CMP;
 using Sitecore.Connector.CMP.Conversion;
--- Website/src/platform/Pipelines/PublishSyncedItem.cs
+++ Website/src/platform/Pipelines/PublishSyncedItem.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using HtmlAgilityPack;
 using Sitecore.Abstractions;
 using Sitecore.Connector.CMP;
-using Sitecore.Connector.CMP.Conversion;
-using Sitecore.Connector.CMP.Helpers;
 using Sitecore.Connector.CMP.Pipelines.ImportEntity;
-using Sitecore.ContentSearch;
-using Sitecore.ContentSearch.SearchTypes;
-using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
-using Sitecore.Globalization;
-using Sitecore.SecurityModel;
 using Sitecore.Data;
 using Sitecore.Publishing;
 
@@ -20,19 +10,13 @@ namespace Sitecore.Demo.Edge.Website.Pipelines
 {
     public class PublishSyncedItem : ImportEntityProcessor
     {
-        private static CmpSettings _settings;
-        protected BaseLog Logger { get; }
-
         public PublishSyncedItem(BaseLog logger, CmpSettings settings)
             : base(logger, settings)
         {
-            this.Logger = logger;
-            PublishSyncedItem._settings = settings;
         }
 
         public override void Process(ImportEntityPipelineArgs args, BaseLog logger)
         {
-
             PublishOptions publishOptions = new PublishOptions(args.Item.Database,
                 Database.GetDatabase("web"), PublishMode.Incremental, args.Item.Language, DateTime.Now);
             publishOptions.RootItem = args.Item;
