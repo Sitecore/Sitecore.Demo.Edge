ce2ff621ff55c0f95b0aff852f971c5d1d496661
Minhaz Topaz <mint@sitecore.net>
2021-10-29 17:13:56 -0600

    responsive resizing

--- tv/components/ScheduleForDay.tsx
+++ tv/components/ScheduleForDay.tsx
@@ -3,6 +3,8 @@ import { Day, ScheduleSlot } from '../interfaces/session';
 import { Carousel } from 'react-responsive-carousel';
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import hallImage from '../public/conference-hall.jpg';
 
 type ScheduleForDayProps = {
   day: string;
@@ -10,45 +12,54 @@ type ScheduleForDayProps = {
   schedule: ScheduleSlot[][];
 };
 
-const ScheduleForDay = (props: ScheduleForDayProps): JSX.Element => {
-  return (
-    <>
-      <ul className="absolute p-5 top-0 left-0 z-50 text-black-lightest">
-        {props.days.map((day, index) => (
-          <li key={index}>
-            <Link href={'/schedule/' + day.sortOrder}>
-              <a> {day.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="conference-hall">
-        <div className="schedule">
-          <Carousel
-            autoPlay={false}
-            axis={'vertical'}
-            infiniteLoop={false}
-            interval={8000}
-            showArrows={false}
-            showStatus={false}
-            showIndicators={false}
-            showThumbs={false}
-            useKeyboardArrows={true}
-            verticalSwipe={'natural'}
-          >
-            {props.schedule &&
-              props.schedule.map((value, index) => (
-                <div key={index}>
-                  {value.map((session, i) => (
-                    <ScheduleRow sessions={session.Sessions} timeslot={session.Timeslot} key={i} />
+const ScheduleForDay = (props: ScheduleForDayProps): JSX.Element => (
+  <>
+    <ul className="absolute p-5 top-0 left-0 z-50 text-black-lightest">
+      {props.days.map((day, index) => (
+        <li key={index}>
+          <Link href={'/schedule/' + day.sortOrder}>
+            <a> {day.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <div className="conference-hall">
+      <Image className="image" src={hallImage} alt="asda" layout="responsive" />
+      <div id="container">
+        <div id="monitor">
+          <div id="monitorscreen">
+            <div className="schedule">
+              <Carousel
+                autoPlay={false}
+                axis={'vertical'}
+                infiniteLoop={false}
+                interval={10000}
+                showArrows={false}
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+                useKeyboardArrows={true}
+                verticalSwipe={'natural'}
+              >
+                {props.schedule &&
+                  props.schedule.map((value, index) => (
+                    <div key={index}>
+                      {value.map((session, i) => (
+                        <ScheduleRow
+                          sessions={session.Sessions}
+                          timeslot={session.Timeslot}
+                          key={i}
+                        />
+                      ))}
+                    </div>
                   ))}
-                </div>
-              ))}
-          </Carousel>
+              </Carousel>
+            </div>
+          </div>
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
 export default ScheduleForDay;
