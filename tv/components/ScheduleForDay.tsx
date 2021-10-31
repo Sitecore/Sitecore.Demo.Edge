74e2afc0e8644030c8035237f7af1fb17e1fb1ba
Minhaz Topaz <mint@sitecore.net>
2021-10-31 05:35:59 -0600

    vanilla js carousel added
    TODO: fix timing issue

--- tv/components/ScheduleForDay.tsx
+++ tv/components/ScheduleForDay.tsx
@@ -1,6 +1,6 @@
 import ScheduleRow from './ScheduleRow';
 import { Day, ScheduleSlot } from '../interfaces/session';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import hallImage from '../public/conference-hall.jpg';
@@ -12,6 +12,26 @@ type ScheduleForDayProps = {
 };
 
 const ScheduleForDay = (props: ScheduleForDayProps): JSX.Element => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    function handleCarousel() {
+      console.log('timing out' + Date.now());
+      const slidecontainers = document.querySelectorAll('.slide-container');
+
+      console.log('activeSlide = ' + activeSlide);
+      const newSlide = activeSlide + 1 >= slidecontainers.length ? 0 : activeSlide + 1;
+
+      setActiveSlide(newSlide);
+      for (let i = 0; i < slidecontainers.length; i++) {
+        slidecontainers[i].classList.remove('active');
+      }
+      slidecontainers.item(activeSlide).classList.add('active');
+    }
+
+    setInterval(handleCarousel, 10000);
+  }, [activeSlide]);
+
   return (
     <>
       <ul className="absolute p-5 top-0 left-0 z-50 text-black-lightest">
@@ -32,6 +52,7 @@ const ScheduleForDay = (props: ScheduleForDayProps): JSX.Element => {
                 {props.schedule &&
                   props.schedule.map((value, index) => (
                     <div
+                      id={'slide' + index}
                       className={index == 0 ? 'slide-container active' : 'slide-container'}
                       key={index}
                     >
