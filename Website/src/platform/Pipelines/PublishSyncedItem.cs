1bb8f30f947788a0006bc0bc3259931a38648ee5
Scott Mulligan <scm@sitecore.net>
2021-11-02 17:13:46 -0400

    Updated publishing code and uses PublishMode.SingleItem to only publish the item synced from Content Hub

--- Website/src/platform/Pipelines/PublishSyncedItem.cs
+++ Website/src/platform/Pipelines/PublishSyncedItem.cs
@@ -5,6 +5,7 @@ using Sitecore.Connector.CMP.Pipelines.ImportEntity;
 using Sitecore.Diagnostics;
 using Sitecore.Data;
 using Sitecore.Publishing;
+using Sitecore.Configuration;
 
 namespace Sitecore.Demo.Edge.Website.Pipelines
 {
@@ -17,15 +18,18 @@ namespace Sitecore.Demo.Edge.Website.Pipelines
 
         public override void Process(ImportEntityPipelineArgs args, BaseLog logger)
         {
-            PublishOptions publishOptions = new PublishOptions(args.Item.Database,
-                Database.GetDatabase("web"), PublishMode.Incremental, args.Item.Language, DateTime.Now);
-            publishOptions.RootItem = args.Item;
-            publishOptions.Deep = true;
-            publishOptions.PublishRelatedItems = true;
-            publishOptions.CompareRevisions = true;
+            var source = args.Item.Database;
+            var target = Factory.GetDatabase("web");
 
-            var handle = PublishManager.Publish(new PublishOptions[] { publishOptions });
-            PublishManager.WaitFor(handle);
+            PublishOptions publishOptions = new PublishOptions(source, target, PublishMode.SingleItem, args.Item.Language, DateTime.Now)
+            {
+                RootItem = args.Item,
+                Deep = true,
+                PublishRelatedItems = true
+            };
+
+            var publisher = new Publisher(publishOptions);
+            publisher.PublishAsync();
 
             Log.Info("DEMO CUSTOMIZATION : Published item " + args.Item.Name + " with id: " + args.Item.ID, this);
         }
