6f6c8a917a84d4f7c682acc50508cd3d0faac3cb
Jeff L'Heureux <jel@sitecore.net>
2021-11-04 15:18:26 -0400

    Move schedule page array utilities

--- tv/pages/schedule/[id].tsx
+++ tv/pages/schedule/[id].tsx
@@ -1,8 +1,8 @@
 import { GetAllDays, getAllSessionsByDay } from '../../api/queries/getSessions';
 import { Day } from '../../interfaces/day';
 import { ScheduleSlot } from '../../interfaces/schedule';
+import { Session } from '../../interfaces/session';
 import { Params } from '../../interfaces';
-import { groupBy, SplitArray } from '../../utilities/arrayUtilities';
 import React from 'react';
 import ScheduleForDay from '../../components/ScheduleForDay';
 
@@ -43,9 +43,41 @@ export const getStaticProps = async ({ params }: ScheduleParams) => {
   return {
     props: {
       days: days,
-      schedule: SplitArray(groupBy(sessions), 3),
+      schedule: splitArray(groupBy(sessions), 3),
       day: params.id,
     },
     revalidate: 10,
   };
 };
+
+function groupBy(original: Session[]): ScheduleSlot[] {
+  let currentOutputIndex = -1;
+  const initialOutput: ScheduleSlot[] = [];
+
+  return original.reduce((output: ScheduleSlot[], currentValue: Session) => {
+    // Create a new ScheduleSlot if needed
+    if (output.length === 0 || currentValue.timeslot !== output[currentOutputIndex].Timeslot) {
+      currentOutputIndex += 1;
+      output.push({
+        Timeslot: currentValue.timeslot,
+        Sessions: [],
+      });
+    }
+
+    // Add current session to ScheduleSlot
+    output[currentOutputIndex].Sessions.push(currentValue);
+
+    return output;
+  }, initialOutput);
+}
+
+function splitArray(original: ScheduleSlot[], chunkSize: number): ScheduleSlot[][] {
+  const returnArray = [];
+
+  for (let i = 0; i < original.length; i += chunkSize) {
+    const temporary = original.slice(i, i + chunkSize);
+    returnArray.push(temporary);
+  }
+
+  return returnArray;
+}
