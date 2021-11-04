3e057b10f665f533bf78a1d2e89dc71a04e850e7
Minhaz Topaz <82103130+mintsitecore@users.noreply.github.com>
2021-11-04 14:38:13 -0600

    Update index.ts

--- billboard/api/index.ts
+++ billboard/api/index.ts
@@ -1,6 +1,6 @@
 export async function fetchGraphQL(query: string): Promise<unknown> {
-  const apiKey: string = process.env.NEXT_PUBLIC_CMP_PREVIEW_API_KEY || 'QmN2bkkvWFdVWlg0Z2NMUE1MRENQZUFWVGF2MS8xWCsvQTlNdTMzeGRWVT18cGxheXN1bW1pdA==';
-  const endpointUrl: string = process.env.NEXT_PUBLIC_CMP_PREVIEW_ENDPOINT_URL || 'https://playsummit.sitecoresandbox.cloud';
+  const apiKey: string = process.env.NEXT_PUBLIC_CMP_PREVIEW_API_KEY || '';
+  const endpointUrl: string = process.env.NEXT_PUBLIC_CMP_PREVIEW_ENDPOINT_URL || '';
 
   try {
     const result = await fetch(endpointUrl + '/api/graphql/preview/v1', {
