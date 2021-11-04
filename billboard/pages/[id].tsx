18536a7f9fb17022925bb8f77cc1a2b6c0e17243
Minhaz Topaz <mint@sitecore.net>
2021-11-04 11:29:23 -0600

    realistic billboard frame added

--- billboard/pages/[id].tsx
+++ billboard/pages/[id].tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { contentHubImageSrcGenerator } from "../utilities/contentHubImageLoader";
 import React from "react";
 import Navigation from "../components/Navigation";
-import theBillboard from "../public/billboard.png";
+import theBillboard from "../public/billboard-frame.png";
 import Link from "next/link";
 
 type BillboardProps = {
@@ -41,45 +41,48 @@ export default function BillboardPage(props: BillboardProps) {
             height={900}
             layout={"fixed"}
           ></Image>
-        </div>
-        <div className="billboard-container">
-          <div
-            className="image-left"
-            style={{
-              backgroundImage:
-                "url(" +
-                contentHubImageSrcGenerator(
-                  props.billboard.advertisement_Image
-                ) +
-                ")",
-            }}
-          ></div>
-          <div className="content-right">
-            <div className="logo">
-              <Image
-                src={contentHubImageSrcGenerator(
-                  props.billboard.advertisement_Logo
-                )}
-                alt={props.billboard.content_Name}
-                height={150}
-                width={400}
-              />
-            </div>
-            <div className="slogan">{props.billboard.advertisement_Slogan}</div>
-            <div className="eyebrow">
-              {props.billboard.advertisement_Eyebrow}
-            </div>
-            <div className="title">{props.billboard.advertisement_Title}</div>
+          <div className="billboard-container">
             <div
-              dangerouslySetInnerHTML={{
-                __html: props.billboard.advertisement_Body,
+              className="image-left"
+              style={{
+                backgroundImage:
+                  "url(" +
+                  contentHubImageSrcGenerator(
+                    props.billboard.advertisement_Image
+                  ) +
+                  ")",
               }}
-              className="body"
             ></div>
+            <div className="content-right">
+              <div className="logo">
+                <Image
+                  src={contentHubImageSrcGenerator(
+                    props.billboard.advertisement_Logo
+                  )}
+                  alt={props.billboard.content_Name}
+                  height={150}
+                  width={400}
+                />
+              </div>
+              <div className="slogan">
+                {props.billboard.advertisement_Slogan}
+              </div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: props.billboard.advertisement_Eyebrow,
+                }}
+                className="eyebrow"
+              ></div>
+              <div className="title">{props.billboard.advertisement_Title}</div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: props.billboard.advertisement_Body,
+                }}
+                className="body"
+              ></div>
+            </div>
           </div>
         </div>
-
-        <div className="cylinder"></div>
       </div>
     </>
   );
@@ -95,7 +98,6 @@ export async function getStaticPaths() {
   return { paths, fallback: false };
 }
 
-// This also gets called at build time
 export const getStaticProps = async ({ params }: BillboardParams) => {
   const { billboard } = await getBillboardById(params.id);
 
