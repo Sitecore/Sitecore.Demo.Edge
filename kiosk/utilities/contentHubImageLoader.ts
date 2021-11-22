f0b0a85ac24fb0cfa6a57b3332ab8fda0d68894e
Jeff L'Heureux <jel@sitecore.net>
2021-11-22 14:29:54 -0500

    Remove unused contentHubImageLoader

--- kiosk/utilities/contentHubImageLoader.ts
+++ kiosk/utilities/contentHubImageLoader.ts
@@ -1,16 +1,10 @@
 import { Image } from '../interfaces';
 
-interface Props {
-  src: string;
-}
-
 const contentHubUrl = process.env.NEXT_PUBLIC_CMP_PREVIEW_ENDPOINT_URL || '';
-
-export const contentHubImageLoader = ({ src }: Props) => {
-  return `${contentHubUrl}/api/public/content/${src}`;
-};
 
 //TODO: Remove the 8443 port number when content hub is fixed
 export const contentHubImageSrcGenerator = (src: Image) => {
-  return `${contentHubUrl}:8443/api/public/content/${src.results[0].assetToPublicLink.results[0].relativeUrl}?v=${src.results[0].assetToPublicLink.results[0].versionHash}`;
+  const image = src.results[0].assetToPublicLink.results[0];
+
+  return `${contentHubUrl}:8443/api/public/content/${image.relativeUrl}?v=${image.versionHash}`;
 };
