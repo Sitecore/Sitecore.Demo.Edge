6028cfde1c143573448780fbf839b46ce0ca369e
Minhaz Topaz <mint@sitecore.net>
2021-11-25 00:35:01 -0700

    making room for session detail

--- kiosk/components/ScheduleRow.tsx
+++ kiosk/components/ScheduleRow.tsx
@@ -6,6 +6,11 @@ type ScheduleRowProps = {
   timeslot: string;
 };
 
+const showDetail = (sessionId: string) => {
+  console.log('button clicked ==================> ' + sessionId);
+  document.getElementById(sessionId)?.classList.add('active');
+};
+
 const ScheduleRow = (props: ScheduleRowProps): JSX.Element => {
   return (
     <>
@@ -14,10 +19,11 @@ const ScheduleRow = (props: ScheduleRowProps): JSX.Element => {
         const premiumClass = session.isPremium ? ' premium-session ' : '';
         const isKeynoteSession = session.type?.toLowerCase() === 'keynote';
         const keynoteClass = session.type?.toLowerCase() === 'keynote' ? ' keynote-session ' : '';
-        const noSpeakerClass = session.speaker ? '' : ' no-speaker';
+        const noSpeakerClass = session.speaker ? '' : ' no-speaker ';
 
         return (
           <div
+            onClick={() => showDetail(session.id)}
             className={'schedule-sessions' + premiumClass + keynoteClass + noSpeakerClass}
             key={index}
           >
@@ -33,6 +39,28 @@ const ScheduleRow = (props: ScheduleRowProps): JSX.Element => {
             <div className="session-content">
               <div className="session-name">{session.name}</div>
               <div>{session.room}</div>
+            </div>
+
+            <div
+              id={session.id}
+              className={
+                'hidden absolute left-0 right-0 -top-24 bottom-0 w-full h-full z-50 bg-red session-detail' +
+                premiumClass +
+                keynoteClass +
+                noSpeakerClass
+              }
+            >
+              <div className="session-close-detail top-0 right-0 w-14 h-14 absolute">X</div>
+              <div
+                className={
+                  'session-detail-image w-full h-1/2 bg-cover bg-no-repeat bg-center' +
+                  premiumClass +
+                  keynoteClass
+                }
+                style={{
+                  backgroundImage: `url("${contentHubImageSrcGeneratorFromString(session.image)}")`,
+                }}
+              ></div>
             </div>
           </div>
         );
