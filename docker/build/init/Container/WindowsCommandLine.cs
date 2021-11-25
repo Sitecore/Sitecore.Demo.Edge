2ef49103e5f122ce690fe14f3a717f5441c28f49
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-11-25 14:18:30 -0500

    Update WindowsCommandLine.cs

--- docker/build/init/Container/WindowsCommandLine.cs
+++ docker/build/init/Container/WindowsCommandLine.cs
@@ -18,6 +18,7 @@ namespace Sitecore.Demo.Init.Container
             cmd.StartInfo.FileName = "cmd.exe";
             cmd.StartInfo.RedirectStandardInput = true;
             cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.RedirectStandardError = true;
             cmd.StartInfo.CreateNoWindow = true;
             cmd.StartInfo.UseShellExecute = false;
             cmd.StartInfo.WorkingDirectory = workingDirectory;
@@ -27,9 +28,11 @@ namespace Sitecore.Demo.Init.Container
             cmd.StandardInput.Flush();
             cmd.StandardInput.Close();
             var output = cmd.StandardOutput.ReadToEnd();
+            output += cmd.StandardError.ReadToEnd();
             cmd.WaitForExit();
 
             output += cmd.StandardOutput.ReadToEnd();
+            output += cmd.StandardError.ReadToEnd();
             Console.WriteLine(output);
             return output;
         }
