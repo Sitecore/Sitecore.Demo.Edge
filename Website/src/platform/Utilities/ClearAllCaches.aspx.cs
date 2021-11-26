80daa514760842e8f154ac897abd5b59bc27a059
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-11-26 11:40:42 -0500

    Update ClearAllCaches.aspx.cs

--- Website/src/platform/Utilities/ClearAllCaches.aspx.cs
+++ Website/src/platform/Utilities/ClearAllCaches.aspx.cs
@@ -3,10 +3,10 @@ using System.Web;
 
 namespace Sitecore.Demo.Edge.Website.Utilities
 {
-	public partial class ClearAllCaches : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class ClearAllCaches : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
             Sitecore.Caching.CacheManager.ClearAllCaches();
         }
     }
