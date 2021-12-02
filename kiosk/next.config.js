cf2bd7077540468515d18bf2fce4a5d76bdc4567
Alexander Doroshenko <adoprog@users.noreply.github.com>
2021-12-02 12:31:14 -0500

    AB#6855 Workaround for CH domain limitation

--- kiosk/next.config.js
+++ kiosk/next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  // TODO: remove demoedge once its discontinued
-  images: {
-    domains: ['demoedge.sitecoresandbox.cloud', 'playsummit.sitecoresandbox.cloud', 'playsummit-demo.sitecoresandbox.cloud'],
-  },
 };
--- kiosk/utilities/contentHubImageLoader.ts
+++ kiosk/utilities/contentHubImageLoader.ts
@@ -5,9 +5,9 @@ const contentHubUrl = process.env.NEXT_PUBLIC_CMP_PREVIEW_ENDPOINT_URL || '';
 export const contentHubImageSrcGenerator = (src: Image) => {
   const image = src.results[0].assetToPublicLink.results[0];
 
-  return `${contentHubUrl}/api/public/content/${image.relativeUrl}?v=${image.versionHash}`;
+  return `/api/imgproxy?url=${contentHubUrl}/api/public/content/${image.relativeUrl}?v=${image.versionHash}`;
 };
 
 export const contentHubImageSrcGeneratorFromString = (src: string) => {
-  return `${contentHubUrl}/api/public/content/${src}`;
+  return `/api/imgproxy?url=${contentHubUrl}/api/public/content/${src}`;
 };
