db08361631baa37e601a33156e10c74f1e512815
Minhaz Topaz <git-commit-address>
2022-01-17 14:17:23 -0700

    vendor description page

--- Website/src/rendering/src/components/Vendors/VendorInformation.tsx
+++ Website/src/rendering/src/components/Vendors/VendorInformation.tsx
@@ -1,29 +1,47 @@
 import { ComponentProps } from 'lib/component-props';
 import { Field, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
 import SessionList from '../Sessions/SessionList';
-import { SESSIONS } from '../../models/mock-sessions';
+import { GraphQLSession } from 'src/types/session';
 
 export type VendorInformationProps = ComponentProps & {
   fields: {
-    Description: Field<string>;
+    data: {
+      contextItem: {
+        description: Field<string>;
+        sessions: {
+          targetItems: GraphQLSession[];
+        };
+      };
+    };
   };
 };
 
-const VendorInformation = (props: VendorInformationProps): JSX.Element => (
-  <section className="section information-section">
-    <div className="section__content container">
-      <div className="information-grid">
-        <div className="main-col">
-          <div className="column-title">Vendor history:</div>
-          <RichText className="rich-text" field={props.fields.Description} />
-        </div>
-        <div className="sidebar-col">
-          <div className="column-title">Sessions:</div>
-          <SessionList sessions={SESSIONS} showSpeakers={true} />
+const VendorInformation = (props: VendorInformationProps): JSX.Element => {
+  const SessionListSidebar =
+    props.fields.data.contextItem.sessions.targetItems.length > 0 ? (
+      <>
+        <div className="column-title">Sessions:</div>
+        <SessionList
+          sessions={props.fields.data.contextItem.sessions.targetItems}
+          showSpeakers={true}
+        />
+      </>
+    ) : (
+      <div className="column-title">There are no sessions available yet.</div>
+    );
+
+  return (
+    <section className="section information-section">
+      <div className="section__content container">
+        <div className="information-grid">
+          <div className="main-col">
+            <div className="column-title">Vendor history:</div>
+            <RichText className="rich-text" field={props?.fields?.data?.contextItem?.description} />
+          </div>
+          <div className="sidebar-col">{SessionListSidebar}</div>
         </div>
       </div>
-    </div>
-  </section>
-);
-
+    </section>
+  );
+};
 export default VendorInformation;
