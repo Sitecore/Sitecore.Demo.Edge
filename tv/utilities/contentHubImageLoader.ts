1e067ee1282fbaff856a08cd7589cd3ececb7972
Minhaz Topaz <82103130+mintsitecore@users.noreply.github.com>
2022-01-20 10:28:23 -0700

    Update contentHubImageLoader.ts
    fix lint error

--- tv/utilities/contentHubImageLoader.ts
+++ tv/utilities/contentHubImageLoader.ts
@@ -4,7 +4,6 @@ interface ContentHubImageLoaderProps {
 
 export function contentHubImageSrcGenerator(src: string) {
   const contentHubUrl: string = process.env.NEXT_PUBLIC_CMP_PREVIEW_ENDPOINT_URL || 'https://playsummit.sitecoresandbox.cloud';
-
   return `${contentHubUrl}/api/public/content/${src}`;
 }
 
