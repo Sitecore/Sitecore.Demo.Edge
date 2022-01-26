c8cec9860eaf5aea763e60faa85f2c17d009a50d
Minhaz Topaz <git-commit-address>
2022-01-25 19:13:33 -0700

    keep unused component as is to be removed once new approch is working

--- Website/src/rendering/src/components/Page Content/FullImageCTASection.tsx
+++ Website/src/rendering/src/components/Page Content/FullImageCTASection.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
 
 const FullImageCTASection = (): JSX.Element => (
-  <section
-    className="section section__full-image section__full-image--map"
-    style={{ backgroundImage: 'url(' + news.fields.Image.value?.src + ')' }}
-  >
+  <section className="section section__full-image section__full-image--map">
     <div className="section__content section__full-image__content section__full-image__content--center">
       <Link href="/map">
         <a className="btn--main btn--main--square">Venue Map</a>
