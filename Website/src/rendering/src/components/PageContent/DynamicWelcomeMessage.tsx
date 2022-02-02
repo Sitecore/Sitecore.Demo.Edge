e095288f693ba375a173634dec62f780c610b8ab
Minhaz Topaz <git-commit-address>
2022-02-02 13:12:23 -0700

    close button added

--- Website/src/rendering/src/components/PageContent/DynamicWelcomeMessage.tsx
+++ Website/src/rendering/src/components/PageContent/DynamicWelcomeMessage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getDynamicWelcomeMessage, WelcomeMessage } from 'src/services/BoxeverService';
 import { getIpAddress } from 'src/services/IpAddressService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const DynamicWelcomeMessage = (): JSX.Element => {
   const [message, SetMessage] = useState('');
@@ -11,8 +13,18 @@ const DynamicWelcomeMessage = (): JSX.Element => {
       .then((content: WelcomeMessage) => SetMessage(content.message));
   }, []);
 
+  const CloseWelcomeMessage = () => {
+    console.log('closing message');
+  };
+
   const messageContent = message && (
     <section className="section bx_TopBanner">
+      <FontAwesomeIcon
+        fixedWidth
+        className="sticky-button-right"
+        icon={faTimes}
+        onClick={CloseWelcomeMessage}
+      />
       <div className="section__content container bx_TopBanner__banner">
         <span>{message}</span>
       </div>
