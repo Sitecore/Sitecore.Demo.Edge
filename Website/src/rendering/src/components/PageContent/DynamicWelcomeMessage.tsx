69c2566b7e46b9b1ff03d3684a99eec09aba718d
Minhaz Topaz <git-commit-address>
2022-02-02 12:41:46 -0700

    css added for top bar

--- Website/src/rendering/src/components/PageContent/DynamicWelcomeMessage.tsx
+++ Website/src/rendering/src/components/PageContent/DynamicWelcomeMessage.tsx
@@ -1,22 +1,24 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { getDynamicWelcomeMessage, WelcomeMessage } from 'src/services/BoxeverService';
+import { getIpAddress } from 'src/services/IpAddressService';
 
 const DynamicWelcomeMessage = (): JSX.Element => {
   const [message, SetMessage] = useState('');
 
   useEffect(() => {
-    async function getIpAddress() {
-      const res = await axios.get('https://api.my-ip.io/ip');
-      return res.data;
-    }
-
     getIpAddress()
       .then((ipAddress) => getDynamicWelcomeMessage(ipAddress))
       .then((content: WelcomeMessage) => SetMessage(content.message));
   }, []);
 
-  return <div>{message}</div>;
+  const messageContent = message && (
+    <section className="section bx_TopBanner">
+      <div className="section__content container bx_TopBanner__banner">
+        <span>{message}</span>
+      </div>
+    </section>
+  );
+  return <div>{messageContent}</div>;
 };
 
 export default DynamicWelcomeMessage;
