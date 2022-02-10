788d350f2a0c403d1aab91875a95363a4b2581cb
Jeff L'Heureux <jel@sitecore.net>
2022-02-10 16:40:52 -0500

    Integrate Sitecore Discover SDK.

--- Website/src/rendering/src/services/DiscoverService.tsx
+++ Website/src/rendering/src/services/DiscoverService.tsx
@@ -1,14 +1,33 @@
 import Script from 'next/script';
 
+const DISCOVER_API_KEY = process.env.NEXT_PUBLIC_DISCOVER_API_KEY || '';
 const DISCOVER_CUSTOMER_KEY = process.env.NEXT_PUBLIC_DISCOVER_CUSTOMER_KEY || '';
 const DISCOVER_CUSTOMER_KEY_SUFFIX = !!DISCOVER_CUSTOMER_KEY
   ? DISCOVER_CUSTOMER_KEY.substring(DISCOVER_CUSTOMER_KEY.indexOf('-') + 1)
   : '';
-const isDiscoverConfigured = !!DISCOVER_CUSTOMER_KEY;
+const isDiscoverConfigured = !!DISCOVER_API_KEY && !!DISCOVER_CUSTOMER_KEY;
 
 export const DiscoverScripts: JSX.Element | undefined = isDiscoverConfigured ? (
-  <Script
-    src={`https://${DISCOVER_CUSTOMER_KEY_SUFFIX}-prod.rfksrv.com/rfk/js/${DISCOVER_CUSTOMER_KEY}/init.js`}
-    strategy="lazyOnload"
-  />
+  <>
+    {/* Beacon for old integration way */}
+    <Script
+      src={`https://${DISCOVER_CUSTOMER_KEY_SUFFIX}-prod.rfksrv.com/rfk/js/${DISCOVER_CUSTOMER_KEY}/init.js`}
+      strategy="lazyOnload"
+    />
+
+    {/* SDK for new integration way */}
+    {/* Temporary source for the Discover SDK until it is released on NPM */}
+    <Script
+      src="/rfk-sdk.js"
+      onLoad={() => {
+        RFK.setCredentials({
+          env: 'prod',
+          customerKey: `${DISCOVER_CUSTOMER_KEY}`,
+          apiKey: `${DISCOVER_API_KEY}`,
+          useToken: true,
+        });
+        RFK.init();
+      }}
+    />
+  </>
 ) : undefined;
