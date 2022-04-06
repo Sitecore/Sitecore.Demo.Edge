8478934256a166757ecf563abbaa41aa8a61178c
Scott Mulligan <scm@sitecore.net>
2022-04-06 08:10:16 -0400

    Adding placeholder page for "account"

---
+++ Website/src/rendering/src/pages/account.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { ShopLayout } from 'components/Products/Shop';
+
+const Account = (): JSX.Element => {
+  return (
+    <ShopLayout>
+      <Head>
+        <title>PLAY! Summit - My Account</title>
+      </Head>
+
+      <p>My Account</p>
+    </ShopLayout>
+  );
+};
+
+export default Account;
