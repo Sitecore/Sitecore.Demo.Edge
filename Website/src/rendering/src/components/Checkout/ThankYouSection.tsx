a33ac78776a5bde8804f42332a160dcace491b73
Neli Kostadinova <nko@sitecore.net>
2022-04-13 11:42:53 +0300

    Thank you page styled

--- Website/src/rendering/src/components/Checkout/ThankYouSection.tsx
+++ Website/src/rendering/src/components/Checkout/ThankYouSection.tsx
@@ -1,12 +1,47 @@
+import { faEnvelope, faPrint, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
 const ThankYouSection = (): JSX.Element => (
-  <section className="shop-section thank-you-section">
-    <h1 className="thank-you-title">Thank you for your purchase!</h1>
-    <p>Login or create an account to save your order summary</p>
-    <Link href="/account/login">
-      <a className="btn--main btn--main--round">Log in / Create Account</a>
-    </Link>
+  <section className="thank-you-section">
+    <ul>
+      <li>
+        <span className="item-icon">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </span>
+        <p>
+          An email receipt including the details of your order has been sent to the email address
+          provided. Please keep it for your records.
+        </p>
+      </li>
+      {/* TODO: Different text if user is not logged in */}
+      <li>
+        <span className="item-icon">
+          <FontAwesomeIcon icon={faPrint} />
+        </span>
+        <p>
+          You can visit the <span className="font-medium">&quot;My account&quot;</span> page at any
+          time to check the status of your order. Or{' '}
+          <Link href="#">
+            <a>click here</a>
+          </Link>{' '}
+          to print a copy of your receipt.
+        </p>
+      </li>
+      {/* TODO: Don't show if user is logged in */}
+      <li>
+        <span className="item-icon">
+          <FontAwesomeIcon icon={faUsers} />
+        </span>
+        <p>
+          To follow the state of your order, please{' '}
+          <Link href="/account/login">
+            <a>create an account</a>
+          </Link>
+          .
+        </p>
+      </li>
+    </ul>
   </section>
 );
 
