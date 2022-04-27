64381fe1a4c8d7a9f0357c4198eb479d28dbaf72
Jeff L'Heureux <2000200+jflheureux@users.noreply.github.com>
2022-04-27 06:55:29 -0400

    AB#7442 Add shop footer links back. (#338)

--- Website/src/rendering/src/components/Products/Shop.tsx
+++ Website/src/rendering/src/components/Products/Shop.tsx
@@ -20,11 +20,263 @@ export const ShopLayout = (props: PropsWithChildren<unknown>): JSX.Element => {
             jsonValue: {
               value: {
                 src: 'https://playsummit.sitecoresandbox.cloud/api/public/content/c78f4095acc746a98146aaa38f57a04f?v=85bba949&t=web',
-                width: 413,
-                height: 113,
+                width: '413',
+                height: '113',
               },
             },
-            alt: '',
+            alt: 'PLAY! Summit long light grey',
+          },
+        },
+        links: {
+          displayName: 'Footer',
+          children: {
+            results: [
+              {
+                displayName: 'Follow Us',
+                children: {
+                  results: [
+                    {
+                      displayName: 'Facebook',
+                      icon: { value: 'faFacebookF' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                    {
+                      displayName: 'Youtube',
+                      icon: { value: 'faYoutube' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                    {
+                      displayName: 'Twitter',
+                      icon: { value: 'faTwitter' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                    {
+                      displayName: 'Instagram',
+                      icon: { value: 'faInstagram' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                    {
+                      displayName: 'Linkedin',
+                      icon: { value: 'faLinkedin' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                  ],
+                },
+              },
+              {
+                displayName: 'Pages',
+                children: {
+                  results: [
+                    {
+                      displayName: 'Sessions',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: {
+                          value: {
+                            href: '/en/sessions',
+                            text: '',
+                            anchor: '',
+                            linktype: 'internal',
+                            class: '',
+                            title: '',
+                            target: '',
+                            querystring: '',
+                            id: '{68DC89A4-1B04-59A8-9C4E-3B49D6C61052}',
+                          },
+                        },
+                      },
+                    },
+                    {
+                      displayName: 'Speakers',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: {
+                          value: {
+                            href: '/en/speakers',
+                            text: '',
+                            anchor: '',
+                            linktype: 'internal',
+                            class: '',
+                            title: '',
+                            target: '',
+                            querystring: '',
+                            id: '{1F4B781B-F2A5-5647-99DF-C0C369162C4D}',
+                          },
+                        },
+                      },
+                    },
+                    {
+                      displayName: 'Vendors',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: {
+                          value: {
+                            href: '/en/vendors',
+                            text: '',
+                            anchor: '',
+                            linktype: 'internal',
+                            class: '',
+                            title: '',
+                            target: '',
+                            querystring: '',
+                            id: '{774E44E8-0F30-5879-B847-AD233FFB41AA}',
+                          },
+                        },
+                      },
+                    },
+                    {
+                      displayName: 'Sponsors',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: {
+                          value: {
+                            href: '/en/sponsors',
+                            text: '',
+                            anchor: '',
+                            linktype: 'internal',
+                            class: '',
+                            title: '',
+                            target: '',
+                            querystring: '',
+                            id: '{66C99E47-7BBF-52D1-B1D7-4662B850744A}',
+                          },
+                        },
+                      },
+                    },
+                    {
+                      displayName: 'About Us',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: {
+                          value: {
+                            href: '/en/about-us',
+                            text: '',
+                            anchor: '',
+                            linktype: 'internal',
+                            class: '',
+                            title: '',
+                            target: '',
+                            querystring: '',
+                            id: '{2717574C-48A0-5469-85A8-A332DF71F1E4}',
+                          },
+                        },
+                      },
+                    },
+                    {
+                      displayName: 'News',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: {
+                          value: {
+                            href: '/en/news',
+                            text: '',
+                            anchor: '',
+                            linktype: 'internal',
+                            class: '',
+                            title: '',
+                            target: '',
+                            querystring: '',
+                            id: '{0E4A63DA-7496-557E-BF80-5BD52255E431}',
+                          },
+                        },
+                      },
+                    },
+                    {
+                      displayName: 'Shop',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: {
+                          value: {
+                            href: '/shop',
+                            text: 'shop',
+                            linktype: 'external',
+                            url: '/shop',
+                            anchor: '',
+                            target: '',
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+              {
+                displayName: 'Join Us',
+                children: {
+                  results: [
+                    {
+                      displayName: 'Book Tickets',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                    {
+                      displayName: 'Become a Sponsor',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                    {
+                      displayName: 'Become a Vendor',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                  ],
+                },
+              },
+              {
+                displayName: 'Get Support',
+                children: {
+                  results: [
+                    {
+                      displayName: 'FAQ',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                    {
+                      displayName: 'Tech Support',
+                      icon: { value: '' },
+                      title: { value: '' },
+                      field: {
+                        jsonValue: { value: { href: '' } },
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
           },
         },
       },
