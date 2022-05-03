fc7be4eaf366baa1c94d9ed68eca88b42497ef40
Christos Papoutsakis <chpa@sitecore.net>
2022-05-03 18:55:55 +0300

    refactoring for order checkout types

--- Website/src/rendering/src/models/cdp/OrderCheckoutPayload.ts
+++ Website/src/rendering/src/models/cdp/OrderCheckoutPayload.ts
@@ -1,10 +1,24 @@
-type OrderExtensions = [
-  {
-    name?: string;
-    key?: string;
-    [attribute: string]: string | boolean | number;
-  }
-];
+type OrderExtension = {
+  name?: string;
+  key?: string;
+  [attribute: string]: string | boolean | number;
+};
+
+type OrderCheckoutCommon = {
+  referenceId: string;
+  orderedAt: string;
+  status: string;
+  currencyCode: string;
+  price: number;
+};
+
+export type OrderItem = Partial<OrderCheckoutCommon> & {
+  type?: string;
+  name?: string;
+  productId?: string;
+  quantity?: number;
+  extensions?: OrderExtension[];
+};
 
 export type OrderCheckoutPayload = {
   type: string;
@@ -14,28 +28,10 @@ export type OrderCheckoutPayload = {
   language: string;
   currency: string;
   page: string;
-  order: {
-    referenceId: string;
-    orderedAt: string;
-    status: string;
-    currencyCode: string;
-    price: number;
+  order: OrderCheckoutCommon & {
     paymentType: string;
     cardType: string;
-    extensions?: OrderExtensions;
-    orderItems?: [
-      {
-        type?: string;
-        referenceId?: string;
-        orderedAt?: string;
-        status?: string;
-        currencyCode?: string;
-        price?: number;
-        name?: string;
-        productId?: string;
-        quantity?: number;
-        extensions?: OrderExtensions;
-      }
-    ];
+    extensions?: OrderExtension[];
+    orderItems?: OrderItem[];
   };
 };
