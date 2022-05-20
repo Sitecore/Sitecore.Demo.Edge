f4f1e31775cd1a74bf7cb8ff73c8a42b3ad8e267
Christos Papoutsakis <chpa@sitecore.net>
2022-05-20 20:15:08 +0300

    added CategoryBreadcrumb.tsx

---
+++ Website/src/rendering/src/components/Navigation/CategoryBreadcrumb.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ShopBreadcrumb, { ShopBreadcrumbItem } from './ShopBreadcrumb';
+import { Category, getCategoryChain } from '../../helpers/CategoriesDataHelper';
+
+type CategoryBreadcrumbProps = {
+  category: Category;
+};
+
+const CategoryBreadcrumb = ({ category }: CategoryBreadcrumbProps): JSX.Element => {
+  const categories: Category[] = getCategoryChain(category.ccid);
+
+  // If it's a top-level category we hide the breadcrumb
+  const shopBreadcrumbItems: ShopBreadcrumbItem[] =
+    categories.length > 1
+      ? categories.map((category) => ({
+          displayName: category?.title.length > 0 ? category.title : category.name,
+          urlPath: category.url_path,
+        }))
+      : [];
+
+  return (
+    <>
+      <ShopBreadcrumb
+        rendering={{ componentName: '' }}
+        params={{}}
+        fields={{ items: shopBreadcrumbItems }}
+        isCategoryBreadcrumb={true}
+      />
+    </>
+  );
+};
+
+export default CategoryBreadcrumb;
