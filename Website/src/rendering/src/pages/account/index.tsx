fbace63c5497b544d3fc243ae446b6790774f004
Christos Papoutsakis <chpa@sitecore.net>
2022-05-24 20:30:30 +0300

    cleaned account/index page

--- Website/src/rendering/src/pages/account/index.tsx
+++ Website/src/rendering/src/pages/account/index.tsx
@@ -1,30 +1,7 @@
 import Head from 'next/head';
 import { ShopLayout } from '../../components/Products/Shop';
-import {
-  clearAuthenticationTokens,
-  isLoggedIn,
-  isAuthenticationEnabled,
-  loginUrl,
-  logoutUrl,
-} from '../../services/AuthenticationService';
 
 const Account = (): JSX.Element => {
-  /* eslint-disable @next/next/no-html-link-for-pages */
-  const loginMenuItem = isAuthenticationEnabled && !isLoggedIn && (
-    <div className="shop-navigation-menu-item">
-      <a href={loginUrl}>Login</a>
-    </div>
-  );
-
-  const logoutMenuItem = isAuthenticationEnabled && isLoggedIn && (
-    <div className="shop-navigation-menu-item">
-      <a href={logoutUrl} onClick={clearAuthenticationTokens}>
-        Logout
-      </a>
-    </div>
-  );
-  /* eslint-enable @next/next/no-html-link-for-pages */
-
   return (
     <ShopLayout>
       <Head>
@@ -32,10 +9,6 @@ const Account = (): JSX.Element => {
       </Head>
 
       <p>My Account</p>
-
-      {/* TODO: Move login/logout inside the account popup */}
-      {loginMenuItem}
-      {logoutMenuItem}
     </ShopLayout>
   );
 };
