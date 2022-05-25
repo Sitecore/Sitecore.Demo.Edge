a615c5b1a3016c7332c00f5cfb2f8f89945a99bc
Crhistian <cramirez@four51.com>
2022-05-25 09:57:39 -0500

    oidc fixes

--- Website/src/rendering/src/pages/account/index.tsx
+++ Website/src/rendering/src/pages/account/index.tsx
@@ -12,7 +12,7 @@ const Account = (): JSX.Element => {
   /* eslint-disable @next/next/no-html-link-for-pages */
   const loginMenuItem = isAuthenticationEnabled && !isLoggedIn && (
     <div className="shop-navigation-menu-item">
-      <a href={getLoginUrl()}>Login</a>
+      <a href={getLoginUrl('/shop')}>Login</a>
     </div>
   );
 
