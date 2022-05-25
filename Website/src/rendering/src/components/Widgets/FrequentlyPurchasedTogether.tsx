a313f542a7c76bd9d828a2ef83c0970158420acf
Max Maher <mmaher@four51.com>
2022-05-25 13:13:11 -0500

    Removing carousel feature, adding links

--- Website/src/rendering/src/components/Widgets/FrequentlyPurchasedTogether.tsx
+++ Website/src/rendering/src/components/Widgets/FrequentlyPurchasedTogether.tsx
@@ -1,11 +1,12 @@
 import { RecommendationWidgetProps } from '@sitecore-discover/ui';
-import { useState } from 'react';
+import Link from 'next/link';
 
 type Product = {
   image_url: string;
   name: string;
   price: string;
   sku: string;
+  product_url: string;
 };
 
 interface FrequentlyPurchasedTogetherProps extends RecommendationWidgetProps {
@@ -13,64 +14,30 @@ interface FrequentlyPurchasedTogetherProps extends RecommendationWidgetProps {
 }
 
 const FrequentlyPurchasedTogether = (props: FrequentlyPurchasedTogetherProps): JSX.Element => {
-  const {
-    available,
-    dispatch,
-    loaded,
-    loading,
-    onNavigationNext,
-    onNavigationPrev,
-    onProductClick,
-    products,
-    rfkID,
-  } = props;
+  const { onProductClick, products, rfkID } = props;
 
-  const onPrevClick = () => {
-    if (index - 1 >= 0) {
-      setIndex(index - 1);
-    }
-    onNavigationPrev({ index, sku: 'PSPGSB', rfkId: 'rfkid_33' });
-  };
-
-  const onNextClick = () => {
-    if (index + 1 <= products?.length - 1) {
-      setIndex(index + 1);
-    }
-    console.log('calling more');
-    onNavigationNext({ index, sku: 'PSPGSB', rfkId: 'rfkid_33' });
-  };
-
-  const [index, setIndex] = useState(0);
-
-  const perChunk = 4;
-
-  const productGroupings = products?.reduce((result, item, index) => {
-    const chunkIndex = Math.floor(index / perChunk);
-    if (!result[chunkIndex]) {
-      result[chunkIndex] = [];
-    }
-    result[chunkIndex].push(item);
-    return result;
-  }, []);
+  const recommendedProducts = products?.length && (
+    <>
+      {products.map((product: Product) => {
+        return (
+          <Link href={product.product_url} key={product.sku}>
+            <a onClick={() => onProductClick({ sku: product.sku, rfkId: rfkID })}>
+              <div>
+                <img width={200} src={product.image_url} alt={product.name} />
+                <div>{product.name}</div>
+                <div>{product.price}</div>
+              </div>
+            </a>
+          </Link>
+        );
+      })}
+    </>
+  );
 
   return (
     <section>
       Cart Recommendation Widget
-      <button onClick={onPrevClick}>Back</button>
-      {productGroupings?.length &&
-        productGroupings[index].map((product: Product) => {
-          return (
-            <div
-              key={product.name}
-              onClick={() => onProductClick({ sku: products[index].sku, rfkId: rfkID })}
-            >
-              <img width={200} src={product.image_url} alt={product.name} />
-              <div>{product.name}</div>
-              <div>{product.price}</div>
-            </div>
-          );
-        })}
-      <button onClick={onNextClick}>Next</button>
+      {recommendedProducts}
     </section>
   );
 };
