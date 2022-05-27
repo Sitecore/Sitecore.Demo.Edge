7d5b1a696aa8669a0e6826afacb67005dfc8d012
Christos Papoutsakis <96472119+chpa7@users.noreply.github.com>
2022-05-27 23:47:39 +0300

    AB#7514 Implement the shop navigation account popup (#359)

--- Website/src/rendering/src/pages/shop/index.tsx
+++ Website/src/rendering/src/pages/shop/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { logViewEvent } from '../../services/CdpService';
 import { ShopLayout } from '../../components/Products/Shop';
 import CategoriesList from '../../components/Products/CategoriesList';
@@ -10,11 +10,7 @@ const Shop = (): JSX.Element => {
   });
 
   return (
-    <ShopLayout>
-      <Head>
-        <title>PLAY! SHOP</title>
-      </Head>
-
+    <>
       <CategoriesList
         title="Welcome to PLAY! SHOP"
         subtitle="Shop Trending Categories:"
@@ -23,6 +19,18 @@ const Shop = (): JSX.Element => {
       <section className="section">
         <div className="section__content container">Content here</div>
       </section>
+    </>
+  );
+};
+
+Shop.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <ShopLayout>
+      <Head>
+        <title>PLAY! SHOP</title>
+      </Head>
+
+      {page}
     </ShopLayout>
   );
 };
--- Website/src/rendering/src/pages/shop/products/[...sections].tsx
+++ Website/src/rendering/src/pages/shop/products/[...sections].tsx
@@ -1,15 +1,20 @@
 import Head from 'next/head';
+import { ReactElement } from 'react';
 import { ShopLayout } from '../../../components/Products/Shop';
 import ProductDetails from '../../../components/Products/ProductDetails';
 
 const ProductPage = (): JSX.Element => {
+  return <ProductDetails />;
+};
+
+ProductPage.getLayout = function getLayout(page: ReactElement) {
   return (
     <ShopLayout>
       <Head>
         <title>PLAY! SHOP - Product</title>
       </Head>
 
-      <ProductDetails />
+      {page}
     </ShopLayout>
   );
 };
