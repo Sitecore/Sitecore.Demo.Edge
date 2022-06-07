ebbc924a97df17dd155e678ff352d233e82fc712
Crhistian <cramirez@four51.com>
2022-06-07 11:57:46 -0500

    move auth0 settings to .env as defaults

--- Website/src/rendering/src/constants/seeding.ts
+++ Website/src/rendering/src/constants/seeding.ts
@@ -12,10 +12,3 @@ export const PROFILED_LOCATION_ID_SUFFIX = 'PlaySummitProfiled';
 export const PROFILED_LOCATION_NAME = 'Play! Summit Profiled Location';
 export const PROFILED_HEADSTART_CATALOG_ID = 'PlaySummitProfiled';
 export const PROFILED_HEADSTART_CATALOG_NAME = 'Play! Summit - Profiled Catalog';
-
-// Variables for Shared Auth0
-export const SHARED_AUTH0_CLIENT_ID = 'ZpEPH2WiXPDSu3jYG9wUL9e8Kp4hchGb';
-export const SHARED_AUTH0_CLIENT_SECRET =
-  'L4W-kO5HqiEsaOm5LwvvXhlKtXttR3Sjvg3O1-hw7uwslAIhkjW0ZykuVqZz3__l';
-export const SHARED_AUTH0_AUTHORIZATION_ENDPOINT = 'https://playdemo.us.auth0.com/authorize';
-export const SHARED_AUTH0_TOKEN_ENDPOINT = 'https://playdemo.us.auth0.com/oauth/token';
