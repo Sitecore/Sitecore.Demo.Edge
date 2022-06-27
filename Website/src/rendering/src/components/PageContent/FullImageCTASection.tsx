9cf7730b8faef780a8c9bd3158f7cb7df4a4cf57
Neli Kostadinova <nko@sitecore.net>
2022-06-27 13:33:44 +0300

    PR fix

--- Website/src/rendering/src/components/PageContent/FullImageCTASection.tsx
+++ Website/src/rendering/src/components/PageContent/FullImageCTASection.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const FullImageCTASection = (): JSX.Element => (
   <section className="section full-image-section full-image-section-map">
-    <div className="section-content full-image-section-content full-image-section-content--center">
+    <div className="section-content full-image-section-content">
       <Link href="/map">
         <a className="btn-square">Venue Map</a>
       </Link>
