9ef8776fef7420030e8b552e4b488fc6f60b4287
NellyKostadinova <34534939+NellyKostadinova@users.noreply.github.com>
2022-07-06 09:02:10 +0300

    AB#7739 Refactor Product Card, add products to Product List (#420)

--- Website/src/rendering/src/stories/ShopCommon/ProductList.stories.tsx
+++ Website/src/rendering/src/stories/ShopCommon/ProductList.stories.tsx
@@ -40,5 +40,54 @@ Default.args = {
       sku: 'PSPCCHP',
       skuid: 1699340652,
     },
+    {
+      brand: 'CenterCycle',
+      breadcrumbs: 'Activities>Cycling>Equipment>Maintenance',
+      category_names: ['Maintenance'],
+      description:
+        "The Connection Hose and Valve Adapters kit has all the adapters you need to make your pump work with a wide variety of valves. The kit includes:\n- A universal adapter that fits Schrader, Presta, Dunlop, Woods and most other types of valves\n- A tough vinyl hose that's long enough for many pumps\n- An extension hose for those hard-to-reach valves",
+      final_price: '6.99',
+      final_price_min_formatted: 6.99,
+      final_price_max_formatted: 6.99,
+      finalPrice: '6.99',
+      id: 1833673129,
+      image_url:
+        'https://ch.sitecoredemo.com/api/public/content/connection-hose-and-valve-adapters-product?v=20d7e176',
+      name: 'Connection Hose and Valve Adapters',
+      onClick: () => {
+        return null;
+      },
+      price: '6.99',
+      product_group: 'PSPCCCHVA',
+      product_url: '/shop/products/PSPCCCHVA/centercycle-connection-hose-and-valve-adapters',
+      short_description:
+        'Connection Hose and Valve Adapters is a kit that adapts to all types of valves.',
+      sku: 'PSPCCCHVA',
+      skuid: 1432678581,
+    },
+    {
+      brand: 'CenterCycle',
+      breadcrumbs: 'Activities>Cycling>Equipment>Safety',
+      category_names: ['Safety'],
+      description:
+        'This bike lock is a patented, simple and quick to use bike storage system that secures the wheels, saddle and other components of your bike during short stops. It is made from a high-strength polymer and can be used with wider saddles. It weighs 1.3 pounds and comes in at 59.1 inches long.',
+      final_price: '14.99',
+      final_price_min_formatted: 14.99,
+      final_price_max_formatted: 14.99,
+      finalPrice: '14.99',
+      id: 2062170239,
+      image_url:
+        'https://ch.sitecoredemo.com/api/public/content/combination-bike-lock-product?v=b355531d',
+      name: 'Combination Bike Lock',
+      onClick: () => {
+        return null;
+      },
+      price: '14.99',
+      product_group: 'PSPCCCBL',
+      product_url: '/shop/products/PSPCCCBL/centercycle-combination-bike-lock',
+      short_description: 'An affordable and practical coil bike lock for everyday use.',
+      sku: 'PSPCCCBL',
+      skuid: 1163683593,
+    },
   ],
 };
