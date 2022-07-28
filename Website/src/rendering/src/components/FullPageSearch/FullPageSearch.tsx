042265c5523e70dbe2a5072614031721b579308f
Christos Papoutsakis <chpa@sitecore.net>
2022-07-28 20:51:07 +0300

    added support for products to be saved & loaded from sessionStorage

--- Website/src/rendering/src/components/FullPageSearch/FullPageSearch.tsx
+++ Website/src/rendering/src/components/FullPageSearch/FullPageSearch.tsx
@@ -1,9 +1,10 @@
 import { SearchResultsWidgetProps } from '@sitecore-discover/ui';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import debounce from '../../../src/helpers/Debounce';
 import { SearchResultsActions } from '@sitecore-discover/widgets';
 import FullPageSearchContent from './FullPageSearchContent';
 import { getCategoryByUrlPath } from '../../helpers/CategoriesDataHelper';
+import { Product } from '../../models/discover/Product';
 
 export interface FullPageSearchResultsProps extends SearchResultsWidgetProps {
   rfkId: string;
@@ -23,13 +24,19 @@ const FullPageSearch = ({
   sortChoices,
   products,
   facets,
+  numberOfItems,
   dispatch,
   onFacetClick,
   onClearFilters,
   onPageNumberChange,
   onSortChange,
 }: FullPageSearchResultsProps): JSX.Element => {
+  const isCategoryProductListingPage = rfkId === 'rfkid_10';
+
   const category = getCategoryByUrlPath(window.location.pathname);
+
+  const [loadedProducts, setLoadedProducts] = useState([]);
+
   const setKeyphrase: (keyphrase: string) => void = debounce(
     (keyphrase) =>
       dispatch({ type: SearchResultsActions.KEYPHRASE_CHANGED, payload: { keyphrase } }),
@@ -40,6 +47,7 @@ const FullPageSearch = ({
   const onSearchInputChange = (keyphrase: string) => {
     setKeyphrase(keyphrase);
   };
+
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const searchQuery = urlSearchParams.get('q');
@@ -50,11 +58,50 @@ const FullPageSearch = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const onSearchInputChange = (keyphrase: string) => {
-    setKeyphrase(keyphrase);
+  useEffect(() => {
+    if (!loaded && loading) return;
+
+    const productsFromSessionStorage = loadProductsFromSessionStorage();
+
+    let productsToDisplay = [];
+    if (productsFromSessionStorage && products) {
+      productsToDisplay = [...productsFromSessionStorage, ...products];
+      // Filter the products so that we don't include duplicates when refreshing the page
+      productsToDisplay = productsToDisplay.filter(
+        (value: Product, index: number, self: Product[]) =>
+          self.findIndex((v) => v.sku === value.sku) === index
+      );
+    } else if (products) {
+      productsToDisplay = products;
+    } else {
+      return;
+    }
+    setLoadedProducts(productsToDisplay);
+    saveProductsToSessionStorage(productsToDisplay);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [products]);
+
+  const getSessionStorageKey = (): string => {
+    if (isCategoryProductListingPage && keyphrase) {
+      return `${category.ccid} - ${keyphrase} products`;
+    } else if (isCategoryProductListingPage) {
+      return `${category.ccid} products`;
+    } else {
+      return `${keyphrase} products`;
+    }
   };
 
-  const category = getCategoryByUrlPath(window.location.pathname);
+  const saveProductsToSessionStorage = (products: Product[]) => {
+    sessionStorage.setItem(
+      getSessionStorageKey(),
+      JSON.stringify(products)
+    );
+  };
+
+  const loadProductsFromSessionStorage = () =>
+    JSON.parse(
+      sessionStorage.getItem(getSessionStorageKey())
+    );
 
   return (
     <FullPageSearchContent
@@ -68,8 +115,9 @@ const FullPageSearch = ({
       sortType={sortType}
       sortDirection={sortDirection}
       sortChoices={sortChoices}
-      products={products}
+      products={loadedProducts}
       facets={facets}
+      numberOfItems={numberOfItems}
       dispatch={dispatch}
       onFacetClick={onFacetClick}
       onClearFilters={onClearFilters}
