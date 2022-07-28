c89b2c18eafea61af7e36f24c02eb29573f5c1a0
Christos Papoutsakis <chpa@sitecore.net>
2022-07-28 13:12:37 +0300

    updated FullPageSearchContent stories

--- Website/src/rendering/src/stories/FullPageSearch/FullPageSearchContent.stories.tsx
+++ Website/src/rendering/src/stories/FullPageSearch/FullPageSearchContent.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import FullPageSearchContent from '../../components/FullPageSearch/FullPageSearchContent';
 import { mockDiscoverData } from '../mock-discover-data';
+import { getCategoryByUrlPath } from '../../helpers/CategoriesDataHelper';
 
 export default {
   title: 'Components/FullPageSearch/FullPageSearchContent',
@@ -16,19 +17,19 @@ const Template: ComponentStory<typeof FullPageSearchContent> = (args) => (
 export const Default = Template.bind({});
 Default.args = {
   ...mockDiscoverData.fullPageSearchProps,
-  urlPath: '/shop/categories/PSA0/activities',
+  category: getCategoryByUrlPath('/shop/categories/PSA0/activities'),
 };
 
 export const CategoryListingPageFound = Template.bind({});
 CategoryListingPageFound.args = {
   ...mockDiscoverData.fullPageSearchProps,
   rfkId: 'rfkid_10',
-  urlPath: '/shop/categories/PSA0/activities',
+  category: getCategoryByUrlPath('/shop/categories/PSA0/activities'),
 };
 
 export const CategoryListingPageNotFound = Template.bind({});
 CategoryListingPageNotFound.args = {
   ...mockDiscoverData.fullPageSearchProps,
   rfkId: 'rfkid_10',
-  urlPath: '',
+  category: getCategoryByUrlPath(''),
 };
