c208ab9122f70a6067b6affa036b428ba36a4013
Alexander Doroshenko <adoprog@users.noreply.github.com>
2022-08-26 11:45:41 -0400

    AB#7940 Update IP service (#460)

--- Website/src/rendering/src/services/IpAddressService.ts
+++ Website/src/rendering/src/services/IpAddressService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export async function getIpAddress(): Promise<string> {
-  const res = await axios.get('https://api.my-ip.io/ip');
-  return res.data;
+  const res = await axios.get('https://ipv4.jsonip.com');
+  return res.data.ip;
 }
