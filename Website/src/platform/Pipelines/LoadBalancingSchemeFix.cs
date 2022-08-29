efaa32ae53fac1580d191a7c6210a6f175b71d6d
Jeff L'Heureux <jel@sitecore.net>
2022-08-29 15:27:25 -0400

    Ensure media library images are loading without error in development environment

---
+++ Website/src/platform/Pipelines/LoadBalancingSchemeFix.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Pipelines.PreprocessRequest;
+using System.Web;
+
+namespace Sitecore.Demo.Edge.Website.Pipelines
+{
+    /// <summary>
+    /// Fix for media library image requests errors in development.
+    /// See https://sitecore.stackexchange.com/q/31578/171
+    /// </summary>
+    public class LoadBalancingSchemeFix : PreprocessRequestProcessor
+    {
+        public override void Process(PreprocessRequestArgs args)
+        {
+            var scheme = HttpContext.Current?.Request?.Headers?[Settings.LoadBalancingScheme];
+            if (Settings.LoadBalancingEnabled && (scheme?.Contains(",") ?? false))
+            {
+                HttpContext.Current.Request.Headers[Settings.LoadBalancingScheme] = scheme.Split(',')[0];
+            }
+        }
+    }
+}
