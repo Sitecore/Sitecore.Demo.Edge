a2ce721bb14f8813431fec201b873714dad532f1
Christos Papoutsakis <chpa@sitecore.net>
2022-10-17 17:29:51 +0300

    wrap category pages widgets with widgets provider

--- Website/src/rendering/src/pages/shop/categories/[...sections].tsx
+++ Website/src/rendering/src/pages/shop/categories/[...sections].tsx
@@ -1,13 +1,22 @@
+import { WidgetsProvider } from '@sitecore-discover/react';
 import Head from 'next/head';
 import { ReactElement } from 'react';
 import { ShopLayout } from '../../../components/Products/Shop';
 import DiscoverWidget from '../../../components/ShopCommon/DiscoverWidget';
+import DiscoverServiceConfig from '../../../services/DiscoverServiceConfig';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const CategoryPage = (): JSX.Element => {
+  const config = DiscoverServiceConfig();
+
   return (
     <>
-      <DiscoverWidget rfkId="rfkid_10" />
-      <DiscoverWidget rfkId="rfkid_3" />
+      <WidgetsProvider {...config}>
+        <DiscoverWidget rfkId="rfkid_10" />
+        <DiscoverWidget rfkId="rfkid_3" />
+        {/* Temp add react query dev tools for debugging */}
+        <ReactQueryDevtools initialIsOpen />
+      </WidgetsProvider>
     </>
   );
 };
