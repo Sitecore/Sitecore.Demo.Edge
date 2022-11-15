14760cdcde44458e6710e5880b4b92ec177a271d
Christos Papoutsakis <chpa@sitecore.net>
2022-11-15 18:26:16 +0200

    Update SearchControls to handle sort dropdown value changes

--- Website/src/rendering/src/components/FullPageSearch/SearchControls.tsx
+++ Website/src/rendering/src/components/FullPageSearch/SearchControls.tsx
@@ -1,28 +1,41 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 type SearchControlsProps = {
-  sortChoices: unknown[];
-  sortType: unknown;
-  sortDirection: unknown;
+  sortChoices: { order: string; name: string; label: string }[];
+  sortType: string;
+  sortDirection: string;
   onSortChange: (change: SortChangeRequest) => void;
 };
 
 type SortChangeRequest = {
-  sortType: unknown;
-  sortDirection: unknown;
+  sortType: string;
+  sortDirection: string;
 };
 
-const SearchControls = ({ sortChoices, onSortChange }: SearchControlsProps): JSX.Element => {
+const SearchControls = ({
+  sortChoices,
+  sortType,
+  sortDirection,
+  onSortChange,
+}: SearchControlsProps): JSX.Element => {
+  const [sortValue, setSortValue] = useState('featured#desc');
+
   const handleSortChange = ({ target }: ChangeEvent<HTMLSelectElement>) => {
     const sort = (target as HTMLSelectElement).value.split('#');
     onSortChange({ sortType: sort[0], sortDirection: sort[1] });
   };
 
+  useEffect(() => {
+    if (sortType && sortDirection) {
+      setSortValue(`${sortType}#${sortDirection}`);
+    }
+  }, [sortType, sortDirection]);
+
   return (
     <>
       <div className="control-sort">
         <label>Sort by:</label>
-        <select onChange={(event) => handleSortChange(event)}>
+        <select onChange={(event) => handleSortChange(event)} value={sortValue}>
           {sortChoices?.map(({ label, name, order }) => (
             <option value={name + '#' + order} key={name + '#' + order}>
               {label}
