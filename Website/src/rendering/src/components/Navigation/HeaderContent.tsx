946a65723b0ffe47b4cac373a14b683f20b2716c
Christos Papoutsakis <chpa@sitecore.net>
2022-11-23 17:55:21 +0200

    Fix React hydration error in MS Edge because of language display name mismatch

--- Website/src/rendering/src/components/Navigation/HeaderContent.tsx
+++ Website/src/rendering/src/components/Navigation/HeaderContent.tsx
@@ -3,6 +3,7 @@ import { Placeholder, useSitecoreContext } from '@sitecore-jss/sitecore-jss-next
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { ParsedUrlQueryInput } from 'querystring';
+import { useEffect, useState } from 'react';
 import { HeaderProps } from './Header';
 
 export type HeaderContentProps = HeaderProps & {
@@ -14,12 +15,25 @@ export type HeaderContentProps = HeaderProps & {
 const HeaderContent = (props: HeaderContentProps): JSX.Element => {
   const router = useRouter();
   const { sitecoreContext } = useSitecoreContext();
+  const [languageLabels, setLanguageLabels] = useState([]);
 
   const languageNames = new Intl.DisplayNames(['en'], {
     type: 'language',
   });
 
   const languageList = sitecoreContext['Languages'] as NodeJS.Dict<string | string>[];
+
+  useEffect(() => {
+    // Fix for React hydration error in MS Edge because of the display name mismatch for 'fr-CA'
+    const labels = languageList.map((language) =>
+      languageNames.of(language['Name']) === 'French (Canada)'
+        ? 'Canadian French'
+        : languageNames.of(language['Name'])
+    );
+
+    setLanguageLabels(labels);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const changeLanguage = (lang: string) => {
     if (props.pathname && props.asPath && props.query) {
@@ -34,7 +48,7 @@ const HeaderContent = (props: HeaderContentProps): JSX.Element => {
     }
   };
 
-  const languageSelector = languageList && (
+  const languageSelector = languageList && languageLabels.length > 0 && (
     <select
       onChange={(e) => changeLanguage(e.currentTarget.value)}
       className="languagePicker"
@@ -44,7 +58,7 @@ const HeaderContent = (props: HeaderContentProps): JSX.Element => {
         <option
           key={index}
           value={language['Name']}
-          label={languageNames.of(language['Name'])}
+          label={languageLabels[index]}
           className="languageItem"
         >
           {languageNames.of(language['Name'])}
