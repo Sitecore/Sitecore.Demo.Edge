934dfe452ec40e53a81d942a84e39b7f283e8d59
Jeff L'Heureux <2000200+jflheureux@users.noreply.github.com>
2023-01-05 12:21:55 -0500

    AB#8209 Remove imgproxy (#495)

--- kiosk/utilities/contentHubImageLoader.ts
+++ kiosk/utilities/contentHubImageLoader.ts
@@ -5,9 +5,9 @@ const contentHubUrl = process.env.NEXT_PUBLIC_CMP_PREVIEW_ENDPOINT_URL || '';
 export const contentHubImageSrcGenerator = (src: Image) => {
   const image = src.results[0].assetToPublicLink.results[0];
 
-  return `/api/imgproxy?url=${contentHubUrl}/api/public/content/${image.relativeUrl}?v=${image.versionHash}`;
+  return `${contentHubUrl}/api/public/content/${image.relativeUrl}?v=${image.versionHash}`;
 };
 
 export const contentHubImageSrcGeneratorFromString = (src: string) => {
-  return `/api/imgproxy?url=${contentHubUrl}/api/public/content/${src}`;
+  return `${contentHubUrl}/api/public/content/${src}`;
 };
