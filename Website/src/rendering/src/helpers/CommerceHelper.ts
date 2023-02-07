891582c5a3fe84edf10b1dd409faa22cab3f578c
Jeff L'Heureux <2000200+jflheureux@users.noreply.github.com>
2023-02-07 16:07:52 -0500

    AB#8481 Only initialize Discover and update its context on shop pages (#507)

--- Website/src/rendering/src/helpers/CommerceHelper.ts
+++ Website/src/rendering/src/helpers/CommerceHelper.ts
@@ -3,3 +3,7 @@ export const isCommerceEnabled =
   !!process.env.NEXT_PUBLIC_DISCOVER_CUSTOMER_KEY &&
   !!process.env.NEXT_PUBLIC_ORDERCLOUD_BASE_API_URL &&
   !!process.env.NEXT_PUBLIC_ORDERCLOUD_BUYER_CLIENT_ID;
+
+export const isAShopPage = (urlPathName: string): boolean => {
+  return !!urlPathName && (urlPathName.startsWith('/shop') || urlPathName.startsWith('/account'));
+};
