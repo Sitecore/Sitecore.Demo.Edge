777ae89928fba034a4b04b0c68f1e1246589579e
Scott Mulligan <scm@sitecore.net>
2023-02-06 20:56:49 -0500

    First commit - form appearing but not dynamically getting rendering parameter as form ID

---
+++ Website/src/rendering/src/components/Forms/EmbedSendForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmbedSendForm = (): JSX.Element => {
+  return (
+    <section className="section">
+      <div data-mooform-id="4795788a-e3a4-4bae-a7ec-fd4480e0b832"/>
+    </section>
+  );
+};
+
+export default EmbedSendForm;
