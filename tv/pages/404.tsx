f2e69fa608d8d420dfe02585bd775433086a358a
Christos Papoutsakis <chpa@sitecore.net>
2023-06-28 12:26:06 +0300

    Simplify 404 page

--- tv/pages/404.tsx
+++ tv/pages/404.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import ErrorTv from '../components/ErrorTv';
+import Image from 'next/image';
+import staticImage from '../public/tv-static.gif';
 
 const Custom404 = (): JSX.Element => {
-  return <ErrorTv />;
+  return (
+    <div className="conference-hall">
+      <div id="container">
+        <div id="monitor">
+          <div id="monitorscreen">
+            <Image className="image" src={staticImage} alt="Conference lobby" layout="responsive" />
+            <div className="error">
+              <span className="error-icon">☹</span>
+              Let&apos;s just say somehow, somewhere, something went very very wrong... <br />
+              Somebody is looking into it.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Custom404;
