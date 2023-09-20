71a6ed5f38cfdc0d2de438b029edc3bdbe085e86
linoscore <96075133+linoscore@users.noreply.github.com>
2023-09-20 09:48:20 -0600

    Update Home Page Hero.js
    Correct HTML replacement div target (selector was wrong)
    Added CSS compile command and injection

--- demo/experience/personalize/experiences/web/Website - Audience-based website home page hero V2/Home Page Hero.js
+++ demo/experience/personalize/experiences/web/Website - Audience-based website home page hero V2/Home Page Hero.js
@@ -1,3 +1,8 @@
-if (variant.context.decisionOffers.length > 0 && document.getElementById('cdp-audience-based-hero')) {
-	replaceHTML('cdp-audience-based-hero');
-}
+//if (variant.context.decisionOffers.length > 0 && document.getElementById('cdp-audience-based-hero')) {
+    replaceHTML('#cdp-audience-based-home-page-hero');
+    var compiledCSS = Boxever.templating.compile(variant.assets.css)(variant);
+    var styleTag = document.getElementById('style-' + variant.ref);
+    if (styleTag) {
+        styleTag.innerHTML = compiledCSS;
+    }
+//}
