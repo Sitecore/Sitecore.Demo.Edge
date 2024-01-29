9a9a59b8b62d6fc97b212def1068b961203b00e0
Christos Papoutsakis <chpa@sitecore.net>
2024-01-29 15:49:53 +0200

    Update Storybook main.js

--- Website/src/rendering/.storybook/main.js
+++ Website/src/rendering/.storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
 
   addons: [
     '@storybook/addon-links',
@@ -65,6 +65,6 @@ module.exports = {
   },
 
   docs: {
-    autodocs: true,
+    autodocs: false,
   },
 };
