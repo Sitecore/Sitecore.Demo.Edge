b28ae744c350a84785ec9f7f75c357bd8831aed7
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-06-11 10:07:38 -0700

    Upgrade PLAY! Summit to Sitecore 10.4 (#548)

--- tv/pages/rooms/[id].tsx
+++ tv/pages/rooms/[id].tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useRef, useState } from 'react';
 import Router, { NextRouter, useRouter } from 'next/router';
 import { getSessionsByRoom } from '../../api/queries/getSessions';
-import { getRooms } from '../../api/queries/getRooms';
 import { Session } from '../../interfaces/session';
 import { Params } from '../../interfaces';
 import RoomDisplay from '../../components/RoomDisplay';
@@ -109,17 +108,13 @@ const RoomPage = (props: RoomProps) => {
 
 // This function gets called at build time
 export async function getStaticPaths() {
-  // Call an external API endpoint to get rooms
-  const { rooms } = await getRooms();
-
-  // Get the paths we want to pre-render based on rooms
-  const paths = rooms.map((room) => ({
-    params: { id: room.id },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
+  // Return empty paths because we don't want to generate anything on build
+  // { fallback: blocking } will server-render pages
+  // on-demand if the path doesn't exist.
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
 }
 
 // This also gets called at build time
